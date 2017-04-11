fff9adf3be43713e01bf8be7a148efe585a1a840
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-04-11 10:09:10 -0400

    ESLinted isDate utility function

--- src/utils/FormValidationUtils.js
+++ src/utils/FormValidationUtils.js
@@ -1,24 +1,24 @@
 function isDate(input, minYear = 1902, maxYear = new Date().getFullYear()) {
   // regular expression to match required date format
-  const regex = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+  const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
 
   if (input === '') {
     return false;
   }
 
-  if(input.match(regex)) {
+  if (input.match(regex)) {
     const temp = input.split('/');
     const dateFromInput = new Date(`${temp[2]}/${temp[0]}/${temp[1]}`);
 
     return (
-      dateFromInput.getDate() ===  Number(temp[1])
+      dateFromInput.getDate() === Number(temp[1])
       && (dateFromInput.getMonth() + 1) === Number(temp[0])
       && Number(temp[2]) > minYear
       && Number(temp[2]) < maxYear
     );
-  } else {
-    return false;
   }
+
+  return false;
 }
 
 export {
