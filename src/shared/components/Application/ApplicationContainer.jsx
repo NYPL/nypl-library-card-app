c92b7791eae620a38db61e24d3e482e6687a985c
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-04-11 09:14:03 -0400

    Fixed filepaths

---
+++ src/shared/components/Application/ApplicationContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Header, navConfig } from '@nypl/dgx-header-component';
+import Footer from '@nypl/dgx-react-footer';
+import LibraryCardForm from '../../containers/LibraryCardForm/LibraryCardForm';
+
+const ApplicationContainer = () => (
+  <div className="nyplLibCardApp">
+    <Header
+      skipNav={{ target: 'mainContent' }}
+      navData={navConfig.current}
+    />
+    <LibraryCardForm />
+    <Footer
+      id="footer"
+      className="footer"
+    />
+  </div>
+);
+
+export default ApplicationContainer;
