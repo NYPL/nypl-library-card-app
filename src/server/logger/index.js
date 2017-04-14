00d58a87a98d5cf9fc49d232f8da60cf3ff3d8f8
Ho-Ling Poon <holingpoon@nypl.org>
2017-04-14 12:26:20 -0400

    Reformatting indentation.

--- src/server/logger/index.js
+++ src/server/logger/index.js
@@ -5,24 +5,24 @@ winston.emitErrs = false;
 const logLevel = (process.env.NODE_ENV === 'production') ? 'warn' : 'debug';
 
 const logger = new winston.Logger({
-    transports: [
-        new winston.transports.File({
-            level: logLevel,
-            filename: './log/get_a_library_card.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-        }),
-        new winston.transports.Console({
-            level: logLevel,
-            handleExceptions: true,
-            json: true,
-            colorize: true
-        })
-    ],
-    exitOnError: false
+  transports: [
+    new winston.transports.File({
+      level: logLevel,
+      filename: './log/get_a_library_card.log',
+      handleExceptions: true,
+      json: true,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false
+    }),
+    new winston.transports.Console({
+      level: logLevel,
+      handleExceptions: true,
+      json: true,
+      colorize: true
+    })
+  ],
+  exitOnError: false
 });
 
 module.exports = logger;
