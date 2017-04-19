cc89a19ec0ef0210f162d2ef1b1ebae5dc3bcbb2
Ho-Ling Poon <holingpoon@nypl.org>
2017-04-19 18:13:47 -0400

    ACCT-397 Removed banner on Confirmation view.

--- src/shared/containers/LibraryCardForm/LibraryCardForm.jsx
+++ src/shared/containers/LibraryCardForm/LibraryCardForm.jsx
@@ -11,6 +11,7 @@ import FormField from '../../components/FormField/FormField';
 import ErrorBox from '../../components/ErrorBox/ErrorBox';
 
 class LibraryCardForm extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -243,6 +244,7 @@ class LibraryCardForm extends React.Component {
       })
       .then((response) => {
         // Debugging only (Alpha)
+        // logger.info(response.data);
         console.log(response.data);
         this.setState({
           formProcessing: false,
