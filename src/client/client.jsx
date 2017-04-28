5963f32ccc0e33d7f1079b9254e03e8b7ad6d9fa
Kang Peng <kangtingpeng@nypl.org>
2017-04-28 17:44:23 -0400

    Set up client side routes.

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,25 +1,22 @@
 // Polyfill Promise for legacy browsers
 import 'babel-polyfill';
-// import FeatureFlags from 'dgx-feature-flags';
+import FeatureFlags from 'dgx-feature-flags';
 import React from 'react';
 import { render } from 'react-dom';
-// import { Router, useRouterHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import routes from '../shared/routes/routes.jsx';
-// import createBrowserHistory from 'history/lib/createBrowserHistory';
-import ApplicationContainer from '../shared/components/Application/ApplicationContainer';
-// import BarcodeContainer from '../shared/components/BarcodeContainer/BarcodeContainer';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import './styles/main.scss';
 
 window.onload = () => {
-  // const appHistory = useRouterHistory(createBrowserHistory)();
+  const history = useRouterHistory(createBrowserHistory)();
 
-  // if (!window.dgxFeatureFlags) {
-  //   window.dgxFeatureFlags = FeatureFlags.utils;
-  // }
+  if (!window.dgxFeatureFlags) {
+    window.dgxFeatureFlags = FeatureFlags.utils;
+  }
 
   render(
-    // <Router history={appHistory}>{routes}</Router>,
-    <ApplicationContainer />,
-    document.getElementById('nypl-library-card-app'),
+    <Router children={routes} history={history} />,
+    document.getElementById('nypl-library-card-app')
   );
 };
