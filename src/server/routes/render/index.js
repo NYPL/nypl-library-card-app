05cb7fa4bd8c527164948662cce084e1f603f95c
Kang Peng <kangtingpeng@nypl.org>
2017-04-28 12:53:58 -0400

    Moved routes to anohther file.

--- src/server/routes/render/index.js
+++ src/server/routes/render/index.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { Route, RouterContext, match } from 'react-router';
-import ApplicationContainer from '../../../shared/components/Application/ApplicationContainer';
+import { RouterContext, match } from 'react-router';
+import routes from './../../../shared/routes/routes.jsx';
 import config from '../../../../appConfig';
 
 export default function renderApp(req, res) {
   const isProduction = process.env.NODE_ENV === 'production';
   const csrfToken = req.csrfToken();
-  const routes = (
-    <Route path='/library-card' component={ApplicationContainer}>
-    </Route>
-  );
 
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
-      res.status(500).send(error.message)
+      res.status(500).send(error.message);
     } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
     } else if (renderProps) {
       const html = renderToString(<RouterContext {...renderProps} />);
-      const safePath = req.path.replace(/'/g, '').replace(/"/g, '')
+      const safePath = req.path.replace(/'/g, '').replace(/"/g, '');
 
       res
         .status(200)
         .render('index', {
           app: html,
-          isProduction: isProduction,
+          isProduction,
           path: safePath,
           appTitle: config.appTitle,
           favicon: config.favIconPath,
           csrfToken,
         });
     } else {
-      res.status(404).send('Not found')
+      res.status(404).send('Not found');
     }
   });
 }
--- src/shared/routes/routes.jsx
+++ src/shared/routes/routes.jsx
@@ -1,8 +1,7 @@
-import React     from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 import ApplicationContainer from './../components/Application/ApplicationContainer';
 
 export default (
-  <Route name="app" component={ApplicationContainer} path="/">
-  </Route>
+  <Route path="/library-card" component={ApplicationContainer} />
 );
