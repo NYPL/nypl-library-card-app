065012069c6db73b749f9404312138baa47c1a3d
Kang Peng <kangtingpeng@nypl.org>
2017-05-16 15:46:13 -0400

    Updated confirmation page URL to appConfig.js

--- src/shared/containers/LibraryCardForm/LibraryCardForm.jsx
+++ src/shared/containers/LibraryCardForm/LibraryCardForm.jsx
@@ -8,9 +8,16 @@ import { isEmail, isLength, isAlphanumeric } from 'validator';
 import { isDate } from '../../../utils/FormValidationUtils';
 import Confirmation from '../../components/Confirmation/Confirmation';
 import FormField from '../../components/FormField/FormField';
+<<<<<<< HEAD
 import ApiErrors from '../../components/ApiErrors/ApiErrors';
 
 import ReactDOM from 'react-dom';
+||||||| merged common ancestors
+import ErrorBox from '../../components/ErrorBox/ErrorBox';
+=======
+import ErrorBox from '../../components/ErrorBox/ErrorBox';
+import config from './../../../../appConfig.js';
+>>>>>>> Updated confirmation page URL to appConfig.js
 
 class LibraryCardForm extends React.Component {
   constructor(props) {
@@ -248,7 +255,7 @@ class LibraryCardForm extends React.Component {
           formEntrySuccessful: false,
           apiResults: response.data,
         });
-        window.location.href = `https://www.nypl.org/get-help/library-card/confirmation?patronID=${response.data.response.patron_id}&patronName=${this.getFullName()}`;
+        window.location.href = `${config.confirmationURL.base}?patronID=${response.data.response.patron_id}&patronName=${this.getFullName()}`;
       })
       .catch((error) => {
         this.setState({ formProcessing: false, focusOnResult: true });
