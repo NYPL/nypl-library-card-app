b5f98a3fa8b2e194c212b1a73b555de1853538c7
Ho-Ling Poon <holingpoon@nypl.org>
2017-05-19 11:10:25 -0400

    ACCT-525 Basic code conversion from const to class component.

--- src/shared/components/FormField/FormField.jsx
+++ src/shared/components/FormField/FormField.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 
-const FormField = ({
-  id,
-  className,
-  value,
-  label,
-  type,
-  fieldName,
-  isRequired,
-  errorState,
-  handleOnChange,
-  checked,
-  instructionText,
-  maxLength,
-  onBlur = () => {},
-}) => {
-  const requiredMarkup = isRequired ? <span className="nypl-required-field"> Required</span> : null;
-  const renderInstructionText = (text) => {
+class FormField extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderInstructionText (text) {
     if (!text) {
       return null;
     }
@@ -24,7 +13,7 @@ const FormField = ({
     return (
       <span
         className="nypl-field-status"
-        id={`${id}-status`}
+        id={`${this.props.id}-status`}
         aria-live="assertive"
         aria-atomic="true"
       >
@@ -32,42 +21,46 @@ const FormField = ({
       </span>
     );
   };
-  const renderErrorBox = () => (
-    <div className="nypl-field-status">{errorState[fieldName]}</div>
-  );
 
-  let underInputSuggestion = renderInstructionText(instructionText);
-  let errorClass = '';
+  renderErrorBox() {
+    return (
+      <div className="nypl-field-status">{this.props.errorState[this.props.fieldName]}</div>
+    );
+  };
 
-  if (errorState && errorState[fieldName]) {
-    errorClass = 'nypl-field-error';
-    underInputSuggestion = renderErrorBox();
+  render() {
+    const requiredMarkup = this.props.isRequired ? <span className="nypl-required-field"> Required</span> : null;
+    let errorClass = '';
+    let underInputSuggestion = this.renderInstructionText(this.props.instructionText);
+    if (this.props.errorState && this.props.errorState[this.props.fieldName]) {
+      errorClass = 'nypl-field-error';
+      underInputSuggestion = this.renderErrorBox();
+    }
+    return (
+      <div className={`${this.props.className} ${errorClass}`}>
+        <label htmlFor={this.props.id} id={`${this.props.id}-label`}>
+          {
+            this.props.type === 'checkbox' ?
+              <span className={"visuallyHidden"}>{this.props.label}</span> :
+              <span>{this.props.label}</span>
+          }
+          {requiredMarkup}
+        </label>
+        <input
+          value={this.props.value}
+          type={this.props.type}
+          id={this.props.id}
+          aria-required={this.props.type === 'checkbox' ? null : this.props.isRequired}
+          aria-labelledby={(this.props.instructionText) ? `${this.props.id}-label ${this.props.id}-status` : null}
+          onChange={this.props.handleOnChange}
+          checked={this.props.checked}
+          maxLength={this.props.maxLength || null}
+          onBlur={this.props.onBlur}
+        />
+        {underInputSuggestion}
+      </div>
+    );
   }
-
-  return (
-    <div className={`${className} ${errorClass}`}>
-      <label htmlFor={id} id={`${id}-label`}>
-        {
-          type === 'checkbox' ?
-            <span className={"visuallyHidden"}>{label}</span> :
-            <span>{label}</span>
-        }
-        {requiredMarkup}
-      </label>
-      <input
-        value={value}
-        type={type}
-        id={id}
-        aria-required={type === 'checkbox' ? null : isRequired}
-        aria-labelledby={(instructionText) ? `${id}-label ${id}-status` : null}
-        onChange={handleOnChange}
-        checked={checked}
-        maxLength={maxLength || null}
-        onBlur={onBlur}
-      />
-      {underInputSuggestion}
-    </div>
-  );
 };
 
 FormField.propTypes = {
