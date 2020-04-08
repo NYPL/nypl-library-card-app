5297fc16e86168546a24f0c9d3bd10efe8204bd5
Adriana Alter <adrianaalter@nypl.org>
2020-04-08 17:05:18 -0400

    First pass

--- src/server/logger/index.js
+++ src/server/logger/index.js
@@ -1,8 +1,8 @@
 import winston from 'winston';
-// Unexpected Errors will be handeled
+// Unexpected Errors will be handled
 winston.emitErrs = false;
 
-const logLevel = (process.env.NODE_ENV === 'production') ? 'info' : 'debug';
+const logLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
 const logger = new winston.Logger({
   transports: [
@@ -11,19 +11,19 @@ const logger = new winston.Logger({
       filename: './log/get_a_library_card.log',
       handleExceptions: true,
       json: true,
-      maxsize: 5242880, //5MB
+      maxsize: 5242880, // 5MB
       maxFiles: 5,
-      colorize: false
+      colorize: false,
     }),
     new winston.transports.Console({
       level: logLevel,
       handleExceptions: true,
       json: true,
       stringify: true,
-      colorize: true
-    })
+      colorize: true,
+    }),
   ],
-  exitOnError: false
+  exitOnError: false,
 });
 
 module.exports = logger;
