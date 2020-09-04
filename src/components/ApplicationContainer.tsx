5bb867d9e55ec4e74ef1818baa0a56417a383b7b
Edwin Guzman <edwin.gzmn@gmail.com>
2020-09-04 16:27:18 -0400

    Removing the header to test it's repo in AWS

--- src/components/ApplicationContainer.tsx
+++ src/components/ApplicationContainer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Header, navConfig } from "@nypl/dgx-header-component";
+// import { Header, navConfig } from "@nypl/dgx-header-component";
 import Footer from "@nypl/dgx-react-footer";
 import Banner from "./Banner";
 
 const ApplicationContainer: React.FC = ({ children }) => (
   <div className="nypl-library-card-app">
-    <Header skipNav={{ target: "main-content" }} navData={navConfig.current} />
+    {/* <Header skipNav={{ target: "main-content" }} navData={navConfig.current} /> */}
     <main id="main-content" className="main">
       <div className="content-header">
         <Banner />
