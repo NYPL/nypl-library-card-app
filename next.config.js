5a36e2f9b3244506d2b89990f562474a9b0983bd
Edwin Guzman <edwin.gzmn@gmail.com>
2020-09-15 17:50:22 -0400

    Updating minor text updates.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   webpack: (config, { isServer, webpack }) => {
-    // react-axe should only be bundled when REACT_AXE=true
+    // react-axe should only be bundled when TEST_AXE_ENV=true
     !process.env.TEST_AXE_ENV &&
       config.plugins.push(new webpack.IgnorePlugin(/react-axe$/));
     // Fixes npm packages that depend on `fs` module since
