31b67c9dba3f148a8a30bdae3583965696225518
Edwin Guzman <edwin.gzmn@gmail.com>
2022-01-19 10:23:14 -0500

    Updating prod minified version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { NEXT_PUBLIC_USE_AXE } = process.env;
 
 const PROD_BUILD_ID =
-  "production-0.7.9-ad864352edd60ed383550a497a82dc76f2e71e4a";
+  "production-0.7.11-bdcdaade644171606ca2b3b1d23ba2560181855a";
 
 module.exports = {
   basePath: "/library-card",
