1e20401a83ec7068b941f98ce0d2209f4cb5d1dc
Edwin Guzman <edwin.gzmn@gmail.com>
2022-10-12 17:16:29 -0400

    Adding translations for error messages

--- src/data/apiErrorMessageTranslations.ts
+++ src/data/apiErrorMessageTranslations.ts
@@ -1,28 +1,114 @@
 // Error messages from the API that must be translated.
 export const apiErrorTranslations = {
   "The username is unavailable. Please try another.": {
-    es: "El nombre de usuario no está disponible. Por favor, intente con otro.",
+    es: "Este nombre de usuario no está disponible. Pruebe con otro.",
+    zhcn: "此用户名不可用。请尝试输入其他用户名。",
+    ht: "Non itilizatè sa a pa disponib. Tanpri eseye yon lòt.",
+    ko: "이 사용자명은 사용할 수 없습니다. 다른 이름을 사용해주십시오.",
+    ru: "Данное имя пользователя недоступно. Попробуйте другое.",
+    pl: "Ta nazwa użytkownika jest niedostępna. Spróbuj użyć innej.",
+    fr:
+      "Ce nom d’utilisateur n'est pas disponible. Veuillez essayer un autre nom d'utilisateur.",
+    ur: "صارف کا یہ نام دستیاب نہیں ہے۔ براہِ کرم کوئی اور آزمائیں۔",
+    bn: "এই ব্যবহারকারীর নাম পাওয়া যাবে না। অনুগ্রহ করে অন্য একটি চেষ্টা করুন।",
+    ar: "اسم المستخدم هذا غير متاح. يرجى استخدام اسم مستخدم آخر.",
   },
   "This username is unavailable. Please try another.": {
-    es: "El nombre de usuario no está disponible. Por favor, intente con otro.",
+    es: "Este nombre de usuario no está disponible. Pruebe con otro.",
+    zhcn: "此用户名不可用。请尝试输入其他用户名。",
+    ht: "Non itilizatè sa a pa disponib. Tanpri eseye yon lòt.",
+    ko: "이 사용자명은 사용할 수 없습니다. 다른 이름을 사용해주십시오.",
+    ru: "Данное имя пользователя недоступно. Попробуйте другое.",
+    pl: "Ta nazwa użytkownika jest niedostępna. Spróbuj użyć innej.",
+    fr:
+      "Ce nom d’utilisateur n'est pas disponible. Veuillez essayer un autre nom d'utilisateur.",
+    ur: "صارف کا یہ نام دستیاب نہیں ہے۔ براہِ کرم کوئی اور آزمائیں۔",
+    bn: "এই ব্যবহারকারীর নাম পাওয়া যাবে না। অনুগ্রহ করে অন্য একটি চেষ্টা করুন।",
+    ar: "اسم المستخدم هذا غير متاح. يرجى استخدام اسم مستخدم آخر.",
   },
   "There was a problem with the submission": {
-    es: "",
+    es: "Hubo un problema con el envío.",
+    zhcn: "提交时出现问题",
+    ht: "Te gen yon pwoblèm ak soumisyon an",
+    ko: "제출한 정보에 문제가 있었습니다.",
+    ru: "С отправкой данных возникла проблема",
+    pl: "Wystąpił problem z przesłaniem wniosku.",
+    fr: "Un problème est survenu lors de la soumission.",
+    ur: "جمع کرانے میں ایک مسئلہ تھا۔",
+    bn: "এই জমার সময় একটা ত্রুটি হয়েছে",
+    ar: "حدثت مشكلات تتعلق بعملية الإرسال",
   },
   "There was an error with the ILS.": {
-    es: "Hubo un error con el ILS.",
+    es: "Ocurrió un error con el ILS (Sistema integrado de la biblioteca).",
+    zhcn: "ILS（综合图书馆系统）出现错误。",
+    ht: "Te gen yon erè ak ILS (Sistèm Bibliyotèk Entegre) la.",
+    ko: "ILS(통합 라이브러리 시스템)에 오류가 있었습니다.",
+    ru: "Произошла ошибка в ILS (интегрированной библиотечной системе).",
+    pl: "Wystąpił błąd systemu ILS (Zintegrowany system biblioteczny).",
+    fr:
+      "Une erreur s'est produite avec l'ILS (système intégré de la bibliothèque).",
+    ur: "ILS (انٹیگریٹڈ لائبریری سسٹم) میں ایک نقص پڑ گیا تھا۔",
+    bn: "ILS (ইন্টিগ্রেটেড লাইব্রেরি সিস্টেম)-এর ব্যাপারে ত্রুটি হয়েছে।",
+    ar: "حدث خطأ يتعلق بـ ILS (نظام المكتبة المتكامل).",
   },
   "Usernames should be 5-25 characters, letters or numbers only. Please revise your username.": {
-    es: "",
+    es:
+      "Los nombres de usuario deben tener entre 5 y 25 caracteres, solo letras o números. Revise su nombre de usuario.",
+    zhcn: "用户名需由仅包含字母或数字的 5-25 位字符组成。请修改您的用户名。",
+    ht:
+      "Non itilizatè yo dwe 5-25 karaktè, lèt oswa nimewo sèlman. Tanpri modifye non itilizatè w la.",
+    ko:
+      "사용자명은 5-25자이어야 하며, 문자 또는 숫자만 사용할 수 있습니다. 사용자명을 수정해주십시오.",
+    ru:
+      "Имя пользователя должно содержать 5-25 символов (только буквы или цифры). Проверьте имя пользователя.",
+    pl:
+      "Nazwa użytkownika powinna się składać z 5–25 znaków — tylko liter i cyfr. Zmień nazwę użytkownika.",
+    fr:
+      "Les noms d'utilisateur doivent être composés de 5 à 25 caractères, lettres et chiffres uniquement. Veuillez modifier votre nom d'utilisateur.",
+    ur:
+      "صارف کے نام میں صرف 25-5 کریکٹر، حروف یا نمبر ہونے چاہئیں۔ براہِ کرم اپنے صارف کے نام پر نظر ثانی کریں۔",
+    bn:
+      "ব্যবহারকারীর নাম 2-25টি অক্ষরের হতে হবে, কেবলমাত্র অক্ষর বা সংখ্যা। অনুগ্রহ করে আপনার ব্যবহারকারীর নাম সংশোধন করুন।",
+    ar:
+      "يجب أن تتكون أسماء المستخدمين من 5 إلى 25 حرفًا إنجليزيًا، وتشتمل على حروف أو أرقام فقط. يرجى مراجعة اسم المستخدم الخاص بك.",
   },
   "The ILS could not be requested when attempting to create a patron.": {
-    es: "",
+    es: "No se ha podido solicitar el ILS al intentar crear un usuario.",
+    zhcn: "尝试创建顾客时无法向 ILS 发送请求。",
+    ht: "Yo pat rive mande ILS la lè w t ap eseye kreye yon kont kliyan.",
+    ko: "이용자를 생성하려고 할 때 ILS를 요청할 수 없습니다.",
+    ru: "При попытке создания пользователя запрос ILS не удался.",
+    pl: "Próba utworzenia klienta w systemie ILS się nie powiodła.",
+    fr:
+      "L'ILS n'a pas pu être interrogé lors de la tentative de création d'un client.",
+    ur: "سرپرست بنانے کی کوشش کرتے وقت ILS کی درخواست نہیں کی جا سکتی۔",
+    bn: "পৃষ্ঠপোষক তৈরির চেষ্টার সময় ILS-এর অনুরোধ করা যায়নি।",
+    ar: "لا يمكن طلب ILS في أثناء محاولة إنشاء عضوية.",
   },
   "Invalid request to ILS:": {
-    es: "",
+    es: "Solicitud al ILS inválida:",
+    zhcn: "对 ILS 的请求无效：",
+    ht: "Demann ki pa valab pou ILS:",
+    ko: "잘못된 ILS 요청:",
+    ru: "Недопустимый запрос в ILS:",
+    pl: "Nieprawidłowe żądanie dla systemu ILS:",
+    fr: "Demande invalide à l'ILS :",
+    ur: "ILS سے ناجائز درخواست:",
+    bn: "ILS-এ ভুল অনুরোধ:",
+    ar: "طلب غير صالح إلى ILS:",
   },
   "A server error occurred validating a token.": {
-    es: "Se produjo un error del servidor al validar un token.",
+    es: "Ocurrió un error del servidor al validar un token.",
+    zhcn: "令牌验证时出现服务器错误。",
+    ht: "Yon erè sèvè te fèt pandan yo t ap valide yon senbòl.",
+    ko: "토큰 인증 과정에 서버 오류가 발생했습니다.",
+    ru: "При подтверждении маркера произошла ошибка сервера.",
+    pl: "Podczas weryfikacji tokenu wystąpił błąd serwera.",
+    fr:
+      "Une erreur s'est produite sur le serveur lors de la validation d'un jeton.",
+    ur: "ٹوکن کی توثیق کرنے میں سرور کا نقص پیش آ گیا۔",
+    bn: "টোকেনের বৈধতা নির্ধারণের সময় সার্ভারে ত্রুটি হয়েছে।",
+    ar: "حدث خطأ في الخادم في أثناء التحقق من الرمز المميز.",
   },
   "Cannot validate usernames at this time.": {
     es: "",
