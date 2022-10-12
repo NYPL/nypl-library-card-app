50b73e07877ab141f694a2cbd32bbb87c68a7b92
Edwin Guzman <edwin.gzmn@gmail.com>
2022-10-12 11:00:40 -0400

    Updating prod build id

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { i18n } = require("./next-i18next.config");
 
 const PROD_BUILD_ID =
-  "production-0.7.11-bdcdaade644171606ca2b3b1d23ba2560181855a";
+  "production-0.7.11-586c613bb2acc31f4da1c7c79aa5268525213716";
 
 module.exports = {
   basePath: "/library-card",
