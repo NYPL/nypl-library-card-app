1e11ed401e2b05e0eba71a0cd5620670513ecfae
Vera Kahn <verakahn@nypl.org>
2025-04-04 15:49:01 -0400

    more logs

--- src/utils/csrfUtils.ts
+++ src/utils/csrfUtils.ts
@@ -51,6 +51,9 @@ const validateCsrfToken = (req) => {
   const tokenFromRequestCookie = parseTokenFromPostRequestCookies(req);
   if (!tokenFromRequestBody || !tokenFromRequestCookie) {
     logger.debug(`No csrf token missing in body or header}`);
+    logger.debug(`token from request body: `, tokenFromRequestBody)
+    logger.debug(`token from request cookie: `, tokenFromRequestCookie)
+
     return false;
   }
   const bodyAndCookieTokensMatch =
