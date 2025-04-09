478341424772e0b003db733e6101e5c04b50eb80
Vera Kahn <verakahn@nypl.org>
2025-04-09 13:59:50 -0400

    update prod build id

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require("./next-i18next.config");
 
-const PROD_BUILD_ID = "production-0.10.1-8da52ec";
+const PROD_BUILD_ID = "production-2fc9520";
 
 module.exports = {
   basePath: "/library-card",
