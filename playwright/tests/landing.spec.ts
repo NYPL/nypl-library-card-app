7739cbd16441f6172368ec13689b633aaa390201
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-08-20 13:55:23 -0600

    Renamed files to landing

---
+++ playwright/tests/landing.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+import { LandingPage } from "../pages/landing.page";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("");
+});
+
+test("displays the main heading", async ({ page }) => {
+  const landingPage = new LandingPage(page);
+  await expect(landingPage.mainHeading).toBeVisible();
+});
+
+test("displays the apply heading", async ({ page }) => {
+  const landingPage = new LandingPage(page);
+  await expect(landingPage.applyHeading).toBeVisible();
+});
+
+test("displays get started button", async ({ page }) => {
+  const landingPage = new LandingPage(page);
+  await expect(landingPage.getStartedButton).toBeVisible();
+});
