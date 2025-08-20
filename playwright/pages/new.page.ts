578de77ba731cf861c7b684ce28d7dd4d16a13bb
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-08-20 10:52:47 -0600

    Add remaining languages

--- playwright/pages/new.page.ts
+++ playwright/pages/new.page.ts
@@ -4,6 +4,16 @@ export class NewPage {
   readonly page: Page;
   readonly mainHeading: Locator; // displays on each page
   readonly arabicLanguage: Locator;
+  readonly bengaliLanguage: Locator;
+  readonly chineseLanguage: Locator;
+  readonly englishLanguage: Locator;
+  readonly frenchLanguage: Locator;
+  readonly haitianCreoleLanguage: Locator;
+  readonly koreanLanguage: Locator;
+  readonly polishLanguage: Locator;
+  readonly russianLanguage: Locator;
+  readonly spanishLanguage: Locator;
+  readonly urduLanguage: Locator;
   readonly applyHeading: Locator; // displays only on landing page
   readonly learnMore: Locator;
   readonly cardholderTerms: Locator;
@@ -18,6 +28,18 @@ export class NewPage {
       level: 1,
     });
     this.arabicLanguage = page.getByRole("link", { name: "Arabic" });
+    this.bengaliLanguage = page.getByRole("link", { name: "Bengali" });
+    this.chineseLanguage = page.getByRole("link", { name: "Chinese" });
+    this.englishLanguage = page.getByRole("link", { name: "English" });
+    this.frenchLanguage = page.getByRole("link", { name: "French" });
+    this.haitianCreoleLanguage = page.getByRole("link", {
+      name: "Haitian Creole",
+    });
+    this.koreanLanguage = page.getByRole("link", { name: "Korean" });
+    this.polishLanguage = page.getByRole("link", { name: "Polish" });
+    this.russianLanguage = page.getByRole("link", { name: "Russian" });
+    this.spanishLanguage = page.getByRole("link", { name: "Spanish" });
+    this.urduLanguage = page.getByRole("link", { name: "Urdu" });
     this.applyHeading = page.getByRole("heading", {
       name: "Apply for a Library Card Today in a Few Easy Steps",
       level: 2,
@@ -29,7 +51,7 @@ export class NewPage {
     this.rulesRegulations = page.getByRole("link", {
       name: "Rules and Regulations",
     });
-    this.privacyPolicy = page.getByRole("link", { name: "Privacy Policy" });
+    this.privacyPolicy = page.getByRole("link", { name: "Privacy Policy" }); // QA doesn't have header/footer so this will fail in prod as is
     this.getStartedButton = page.getByRole("link", { name: "Get Started" });
   }
 }
--- playwright/tests/new.spec.ts
+++ playwright/tests/new.spec.ts
@@ -20,16 +20,25 @@ test("displays get started button", async ({ page }) => {
   await expect(newPage.getStartedButton).toBeVisible();
 });
 
-test("displays languages", async ({ page }) => {
+test("displays available languages", async ({ page }) => {
   const newPage = new NewPage(page);
   await expect(newPage.arabicLanguage).toBeVisible();
-  // add more language checks
+  await expect(newPage.bengaliLanguage).toBeVisible();
+  await expect(newPage.chineseLanguage).toBeVisible();
+  await expect(newPage.englishLanguage).toBeVisible();
+  await expect(newPage.frenchLanguage).toBeVisible();
+  await expect(newPage.haitianCreoleLanguage).toBeVisible();
+  await expect(newPage.koreanLanguage).toBeVisible();
+  await expect(newPage.polishLanguage).toBeVisible();
+  await expect(newPage.russianLanguage).toBeVisible();
+  await expect(newPage.spanishLanguage).toBeVisible();
+  await expect(newPage.urduLanguage).toBeVisible();
 });
 
-test("displays links", async ({ page }) => {
+test("displays informational links", async ({ page }) => {
   const newPage = new NewPage(page);
   await expect(newPage.learnMore).toBeVisible();
   await expect(newPage.cardholderTerms).toBeVisible();
   await expect(newPage.rulesRegulations).toBeVisible();
-  await expect(newPage.privacyPolicy).toBeVisible(); // QA doesn't have header/footer so this will fail in prod
+  await expect(newPage.privacyPolicy).toBeVisible();
 });
