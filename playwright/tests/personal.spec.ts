cbb0948996c472e1d2e038ca20f48ac1f4d4f1a4
roseb89 <dz4888@wayne.edu>
2025-08-26 16:30:02 -0400

    personal form error message

--- playwright/tests/personal.spec.ts
+++ playwright/tests/personal.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({page}) => {
 
 });
 
-test('Personal information form', async ({page}) => {
+test('Display personal information form', async ({page}) => {
   const personalPage = new PersonalPage(page);
   await expect(personalPage.firstNameInput).toBeVisible();
   await expect(personalPage.lastNameInput).toBeVisible();
@@ -17,4 +17,20 @@ test('Personal information form', async ({page}) => {
   await expect(personalPage.checkBox).toBeVisible();
   await expect(personalPage.previousButton).toBeVisible();
   await expect(personalPage.nextButton).toBeVisible();
+});
+
+test('error validation for personal info form', async ({page}) => {
+  const personalPage = new PersonalPage(page);
+  await personalPage.firstNameInput.fill('');
+  await personalPage.lastNameInput.fill('');
+  await personalPage.emailInput.fill('');
+  await personalPage.dateOfBirthInput.fill('');
+
+  await personalPage.nextButton.click();
+
+  await expect(personalPage.firstNameErrorMessage).toBeVisible();
+  await expect(personalPage.lastNameErrorMessage).toBeVisible();
+  await expect(personalPage.emailErrorMessage).toBeVisible();
+  await expect(personalPage.dateOfBirthErrorMessage).toBeVisible();
+
 });
