393f3656e9b4ab7d14381aa2441ca5ccdc5952c2
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-08-26 10:42:58 -0600

    Resolve landing and changelog conflicts

--- playwright/tests/landing.spec.ts
+++ playwright/tests/landing.spec.ts
@@ -19,26 +19,3 @@ test("displays get started button", async ({ page }) => {
   const landingPage = new LandingPage(page);
   await expect(landingPage.getStartedButton).toBeVisible();
 });
-
-test("displays available languages", async ({ page }) => {
-  const landingPage = new LandingPage(page);
-  await expect(landingPage.arabicLanguage).toBeVisible();
-  await expect(landingPage.bengaliLanguage).toBeVisible();
-  await expect(landingPage.chineseLanguage).toBeVisible();
-  await expect(landingPage.englishLanguage).toBeVisible();
-  await expect(landingPage.frenchLanguage).toBeVisible();
-  await expect(landingPage.haitianCreoleLanguage).toBeVisible();
-  await expect(landingPage.koreanLanguage).toBeVisible();
-  await expect(landingPage.polishLanguage).toBeVisible();
-  await expect(landingPage.russianLanguage).toBeVisible();
-  await expect(landingPage.spanishLanguage).toBeVisible();
-  await expect(landingPage.urduLanguage).toBeVisible();
-});
-
-test("displays informational links", async ({ page }) => {
-  const landingPage = new LandingPage(page);
-  await expect(landingPage.learnMore).toBeVisible();
-  await expect(landingPage.cardholderTerms).toBeVisible();
-  await expect(landingPage.rulesRegulations).toBeVisible();
-  await expect(landingPage.privacyPolicy).toBeVisible();
-});
