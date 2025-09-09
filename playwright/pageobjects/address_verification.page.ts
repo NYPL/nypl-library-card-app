f68c3b67bac15b8390d41fe76210a241814d84ae
roseb89 <dz4888@wayne.edu>
2025-09-09 15:45:01 -0400

    prettier files, removed duplicated pageObjects folders

--- playwright/pageobjects/address_verification.page.ts
+++ playwright/pageobjects/address_verification.page.ts
@@ -1,26 +1,30 @@
-import {Page, Locator} from '@playwright/test';
+import { Page, Locator } from "@playwright/test";
 
 export class AddressVerificationPage {
-    readonly page: Page;
-    readonly mainHeader: Locator;
-    readonly subHeader: Locator;
-    readonly homeAddressHeader: Locator;
-    readonly addressDisplayed: Locator;
-    readonly verifyRadioButton: Locator;
-    readonly nextButton: Locator;
-    readonly previousButton: Locator;
+  readonly page: Page;
+  readonly mainHeader: Locator;
+  readonly subHeader: Locator;
+  readonly homeAddressHeader: Locator;
+  readonly addressDisplayed: Locator;
+  readonly verifyRadioButton: Locator;
+  readonly nextButton: Locator;
+  readonly previousButton: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.mainHeader = page.locator('h1');
-        this.subHeader = page.locator('h2');
-        this.homeAddressHeader = page.locator('h3');
-        this.addressDisplayed = page.getByText(/^(\d+)\s+([A-Za-z0-9\s#.\-&,']{2,})\s+([A-Za-z\s]{2,}),\s+([A-Z]{2})\s+([0-9]{5}(?:-[0-9]{4})?)$/);
-        this.verifyRadioButton = page.locator('span.radio-input[aria-hidden="true"]');
-        this.previousButton = this.page.getByRole('link', { name: 'Previous' });
-        this.nextButton =  this.page.getByRole('button', { name: 'Next', exact: true })
-
-    }
-
-  
+  constructor(page: Page) {
+    this.page = page;
+    this.mainHeader = page.locator("h1");
+    this.subHeader = page.locator("h2");
+    this.homeAddressHeader = page.locator("h3");
+    this.addressDisplayed = page.getByText(
+      /^(\d+)\s+([A-Za-z0-9\s#.\-&,']{2,})\s+([A-Za-z\s]{2,}),\s+([A-Z]{2})\s+([0-9]{5}(?:-[0-9]{4})?)$/
+    );
+    this.verifyRadioButton = page.locator(
+      'span.radio-input[aria-hidden="true"]'
+    );
+    this.previousButton = this.page.getByRole("link", { name: "Previous" });
+    this.nextButton = this.page.getByRole("button", {
+      name: "Next",
+      exact: true,
+    });
+  }
 }
