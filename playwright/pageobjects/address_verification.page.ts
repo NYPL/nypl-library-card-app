e54bacc9754cfc70842adfa260ff8c41174815b8
roseb89 <dz4888@wayne.edu>
2025-09-11 10:53:10 -0400

    updated locators

--- playwright/pageobjects/address_verification.page.ts
+++ playwright/pageobjects/address_verification.page.ts
@@ -6,18 +6,26 @@ export class AddressVerificationPage {
   readonly subHeader: Locator;
   readonly homeAddressHeader: Locator;
   readonly addressDisplayed: Locator;
+  readonly alternativeAddressDisplayed: Locator;
   readonly verifyRadioButton: Locator;
   readonly nextButton: Locator;
   readonly previousButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.mainHeader = page.locator("h1");
-    this.subHeader = page.locator("h2");
-    this.homeAddressHeader = page.locator("h3");
-    this.addressDisplayed = page.getByText(
+    this.mainHeader = this.page.getByRole("heading", {
+      name: "Apply for a Library Card Online",
+    });
+    this.subHeader = this.page.getByRole("heading", {
+      name: "Step 3 of 5: Address Verification",
+    });
+    this.homeAddressHeader = this.page.getByRole("heading", {
+      name: "Home Address",
+    });
+    this.addressDisplayed = this.page.getByText(
       /^(\d+)\s+([A-Za-z0-9\s#.\-&,']{2,})\s+([A-Za-z\s]{2,}),\s+([A-Z]{2})\s+([0-9]{5}(?:-[0-9]{4})?)$/
     );
+
     this.verifyRadioButton = page.locator(
       'span.radio-input[aria-hidden="true"]'
     );
--- playwright/tests/address_verification.spec.ts
+++ playwright/tests/address_verification.spec.ts
@@ -2,20 +2,15 @@ import { test, expect } from "@playwright/test";
 import { AddressVerificationPage } from "../pageobjects/address_verification.page";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("/library-card/address-verification?&newCard=true");
+  await page.goto("library-card/address-verification?&newCard=true");
 });
 
 test("should display the correct headers", async ({ page }) => {
   const addressVerificationPage = new AddressVerificationPage(page);
-  await expect(addressVerificationPage.mainHeader).toHaveText(
-    "Apply for a Library Card Online"
-  );
-  await expect(addressVerificationPage.subHeader).toHaveText(
-    "Step 3 of 5: Address Verification"
-  );
-  await expect(addressVerificationPage.homeAddressHeader).toHaveText(
-    "Home Address"
-  );
+  await expect(addressVerificationPage.mainHeader).toBeVisible();
+  await expect(addressVerificationPage.subHeader).toBeVisible();
+  await expect(addressVerificationPage.homeAddressHeader).toBeVisible();
+
 });
 
 test("should allow user to verify address", async ({ page }) => {
