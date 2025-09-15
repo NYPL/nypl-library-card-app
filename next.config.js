3e20f9fa49835d1f5160d8fc479b27c9d4ef211e
toxiapo <toxiapo@gmail.com>
2025-09-15 11:31:23 -0400

    build id

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("newrelic");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require("./next-i18next.config");
 
-const PROD_BUILD_ID = "production-1.2.6-1";
+const PROD_BUILD_ID = "production-1.2.6-2";
 
 module.exports = {
   basePath: "/library-card",
