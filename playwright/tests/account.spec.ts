06c5203a40cfcd7db3751741291b2711c967807b
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-09-16 14:26:59 -0600

    Reorganize tests

--- playwright/tests/account.spec.ts
+++ playwright/tests/account.spec.ts
@@ -6,85 +6,97 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/library-card/account?newCard=true");
 });
 
-test("displays all headings", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await expect(accountPage.mainHeading).toBeVisible();
-  await expect(accountPage.stepHeading).toBeVisible();
-  await expect(accountPage.homeLibraryHeading).toBeVisible();
+test.describe("displays all form elements on Account page", () => {
+  test("displays all headings", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await expect(accountPage.mainHeading).toBeVisible();
+    await expect(accountPage.stepHeading).toBeVisible();
+    await expect(accountPage.homeLibraryHeading).toBeVisible();
+  });
+
+  test("displays username and password form", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await expect(accountPage.usernameInput).toBeVisible();
+    await expect(accountPage.availableUsernameButton).toBeVisible();
+    await expect(accountPage.passwordInput).toBeVisible();
+    await expect(accountPage.verifyPasswordInput).toBeVisible();
+    await expect(accountPage.showPasswordCheckbox).toBeVisible();
+  });
+
+  test("displays home library form", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await expect(accountPage.selectHomeLibrary).toBeVisible();
+    await expect(accountPage.cardholderTerms).toBeVisible();
+    await expect(accountPage.rulesRegulations).toBeVisible();
+    await expect(accountPage.privacyPolicy).toBeVisible();
+    await expect(accountPage.acceptTermsCheckbox).toBeVisible();
+  });
+
+  test("displays next and previous buttons", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await expect(accountPage.nextButton).toBeVisible();
+    await expect(accountPage.previousButton).toBeVisible();
+  });
 });
 
-test("displays username and password form", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await expect(accountPage.usernameInput).toBeVisible();
-  await expect(accountPage.availableUsernameButton).toBeVisible();
-  await expect(accountPage.passwordInput).toBeVisible();
-  await expect(accountPage.verifyPasswordInput).toBeVisible();
-  await expect(accountPage.showPasswordCheckbox).toBeVisible();
-});
+test.describe("displays errors for invalid inputs", () => {
+  test("displays errors for required fields", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("");
+    await accountPage.passwordInput.fill("");
+    await accountPage.nextButton.click();
+    await expect(accountPage.usernameError).toBeVisible();
+    await expect(accountPage.passwordError).toBeVisible();
+  });
 
-test("displays home library form", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await expect(accountPage.selectHomeLibrary).toBeVisible();
-  await expect(accountPage.cardholderTerms).toBeVisible();
-  await expect(accountPage.rulesRegulations).toBeVisible();
-  await expect(accountPage.privacyPolicy).toBeVisible();
-  await expect(accountPage.acceptTermsCheckbox).toBeVisible();
-});
+  test("displays error when special characters in username", async ({
+    page,
+  }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("User!@#");
+    await accountPage.nextButton.click();
+    await expect(accountPage.usernameError).toBeVisible();
+  });
 
-test("displays next and previous buttons", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await expect(accountPage.nextButton).toBeVisible();
-  await expect(accountPage.previousButton).toBeVisible();
-});
+  test("displays error when non-Latin characters in username", async ({
+    page,
+  }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("用戶名用戶名");
+    await accountPage.nextButton.click();
+    await expect(accountPage.usernameError).toBeVisible();
+  });
 
-test("displays errors for required fields", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await accountPage.usernameInput.fill("");
-  await accountPage.passwordInput.fill("");
-  await accountPage.nextButton.click();
-  await expect(accountPage.usernameError).toBeVisible();
-  await expect(accountPage.passwordError).toBeVisible();
-});
+  test("displays error when username is unavailable", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("username");
+    await accountPage.availableUsernameButton.click();
+    await accountPage.unavailableUsernameError.waitFor({
+      state: "visible",
+      timeout: 10000,
+    });
+    await expect(accountPage.unavailableUsernameError).toBeVisible();
+  });
 
-test("displays error when username is unavailable", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await accountPage.usernameInput.fill("username");
-  await expect(accountPage.availableUsernameButton).toBeVisible();
-  await expect(accountPage.availableUsernameButton).toBeEnabled();
-  await accountPage.availableUsernameButton.click();
-  // await expect(accountPage.availableUsernameButton).toBeDisabled({ timeout: 10000 });
-  await expect(accountPage.unavailableUsernameError).toBeVisible();
-});
-test("displays message when username is available", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await accountPage.usernameInput.fill("validusername");
-  await expect(accountPage.availableUsernameButton).toBeVisible();
-  await expect(accountPage.availableUsernameButton).toBeEnabled();
-  await accountPage.availableUsernameButton.click();
-  // await expect(accountPage.availableUsernameButton).toBeDisabled({ timeout: 10000 });
-  await expect(accountPage.availableUsername).toBeVisible();
-});
+  test("displays error when passwords do not match", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("ValidUser1");
+    await accountPage.passwordInput.fill("ValidPass1!");
+    await accountPage.verifyPasswordInput.fill("DifferentPass1!");
+    await accountPage.nextButton.click();
+    await expect(accountPage.verifyPasswordError).toBeVisible();
+  });
 
-test("displays error when passwords do not match", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await accountPage.usernameInput.fill("ValidUser1");
-  await accountPage.passwordInput.fill("ValidPass1!");
-  await accountPage.verifyPasswordInput.fill("DifferentPass1!");
-  await accountPage.nextButton.click();
-  await expect(accountPage.verifyPasswordError).toBeVisible();
-});
+  test("displays error when terms are not accepted", async ({ page }) => {
+    const accountPage = new AccountPage(page);
+    await accountPage.usernameInput.fill("ValidUser1");
+    await accountPage.passwordInput.fill("ValidPass1!");
+    await accountPage.verifyPasswordInput.fill("ValidPass1!");
+    await accountPage.nextButton.click();
+    await expect(accountPage.acceptTermsError).toBeVisible();
+  });
 
-test("displays error when terms are not accepted", async ({ page }) => {
-  const accountPage = new AccountPage(page);
-  await accountPage.usernameInput.fill("ValidUser1");
-  await accountPage.passwordInput.fill("ValidPass1!");
-  await accountPage.verifyPasswordInput.fill("ValidPass1!");
-  await accountPage.nextButton.click();
-  await expect(accountPage.acceptTermsError).toBeVisible();
-});
-
-test.describe("displays errors for invalid fields", () => {
-  test("enter too many characters", async ({ page }) => {
+  test("displays error with too many characters", async ({ page }) => {
     const accountPage = new AccountPage(page);
     await accountPage.usernameInput.fill("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     await accountPage.passwordInput.fill("123456789012345678901234567890123");
@@ -93,26 +105,12 @@ test.describe("displays errors for invalid fields", () => {
     await expect(accountPage.passwordError).toBeVisible();
   });
 
-  test("enter too few characters", async ({ page }) => {
+  test("displays error with too few characters", async ({ page }) => {
     const accountPage = new AccountPage(page);
     await accountPage.usernameInput.fill("A");
     await accountPage.passwordInput.fill("1!");
     await accountPage.nextButton.click();
     await expect(accountPage.usernameError).toBeVisible();
     await expect(accountPage.passwordError).toBeVisible();
-  });
-
-  test("enter special characters in username", async ({ page }) => {
-    const accountPage = new AccountPage(page);
-    await accountPage.usernameInput.fill("User!@#");
-    await accountPage.nextButton.click();
-    await expect(accountPage.usernameError).toBeVisible();
-  });
-
-  test("enter non-Latin characters", async ({ page }) => {
-    const accountPage = new AccountPage(page);
-    await accountPage.usernameInput.fill("用戶名用戶名");
-    await accountPage.nextButton.click();
-    await expect(accountPage.usernameError).toBeVisible();
   });
 });
