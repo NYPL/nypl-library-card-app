f8ca17a7053c360f7d98fdcc7e2fb26695734403
roseb89 <dz4888@wayne.edu>
2025-09-19 13:41:57 -0400

    congrats-tests

---
+++ playwright/tests/congrats.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import { CongratsPage } from "../pageobjects/congrats.page";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/library-card/congrats?&newCard=true");
+});
+
+
+test('headings and card details display on "Congrats" page', async ({ page }) => {
+  const congratsPage = new CongratsPage(page);
+  await expect(congratsPage.mainHeading).toBeVisible();
+  await expect(congratsPage.stepHeading).toBeVisible();
+});
+
+test('links display on "Congrats" page', async ({ page }) => {
+  const congratsPage = new CongratsPage(page);
+  await expect(congratsPage.locationsLink).toBeVisible();
+  await expect(congratsPage.photoIdAndProofOfAddressLink).toBeVisible();
+  await expect(congratsPage.getHelpEmailLink).toBeVisible();
+  await expect(congratsPage.loginLink).toBeVisible();
+  await expect(congratsPage.findOutLibraryLink).toBeVisible();
+  await expect(congratsPage.discoverLink).toBeVisible();
+});
+
+
+
