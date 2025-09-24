a932f84a8a2f55d103e02ae13b0bb4a64416197b
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-09-24 13:16:41 -0600

    Remove navigation through app flow tests

--- playwright/tests/alternate-address.spec.ts
+++ playwright/tests/alternate-address.spec.ts
@@ -5,11 +5,11 @@ import { LandingPage } from "../pageobjects/landing.page";
 import { PersonalPage } from "../pageobjects/personal.page";
 import { AddressPage } from "../pageobjects/address.page";
 
-test.describe("displays elements on Alternate Address page", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/library-card/workAddress?newCard=true");
-  });
+test.beforeEach(async ({ page }) => {
+  await page.goto("/library-card/workAddress?newCard=true");
+});
 
+test.describe("displays elements on Alternate Address page", () => {
   test("displays all headings", async ({ page }) => {
     const alternateAddressPage = new AlternateAddressPage(page);
     await expect(alternateAddressPage.mainHeading).toBeVisible();
@@ -34,7 +34,7 @@ test.describe("displays elements on Alternate Address page", () => {
 });
 
 test.describe("enters alternate address", () => {
-  test("enters alternate address", async ({ page }) => {
+  test("enters valid alternate address", async ({ page }) => {
     await page.goto("/library-card/workAddress?newCard=true");
     const alternateAddressPage = new AlternateAddressPage(page);
     await expect(alternateAddressPage.addressHeading).toBeVisible();
@@ -49,109 +49,3 @@ test.describe("enters alternate address", () => {
     await expect(addressVerificationPage.alternateAddressHeader).toBeVisible();
   });
 });
-
-test.describe(
-  "navigates through the application flow to Alternate Address page",
-  () => {
-    test("navigates to Alternate Address page from landing", async ({
-      page,
-    }) => {
-      await page.goto("/library-card/new");
-      const landingPage = new LandingPage(page);
-      await expect(landingPage.mainHeading).toBeVisible();
-      await landingPage.getStartedButton.click();
-
-      const personalPage = new PersonalPage(page);
-      await expect(personalPage.stepHeading).toBeVisible();
-      await personalPage.firstNameInput.fill("Test");
-      await personalPage.lastNameInput.fill("User");
-      await personalPage.dateOfBirthInput.fill("01/01/1990");
-      await personalPage.emailInput.fill("test@example.com");
-      await personalPage.nextButton.click();
-
-      const addressPage = new AddressPage(page);
-      await expect(addressPage.addressHeading).toBeVisible();
-      await addressPage.streetAddressInput.fill("123 Main St");
-      await addressPage.cityInput.fill("New York");
-      await addressPage.stateInput.fill("NY");
-      await addressPage.postalCodeInput.fill("10001");
-      await addressPage.nextButton.click();
-
-      const alternateAddressPage = new AlternateAddressPage(page);
-      await expect(alternateAddressPage.addressHeading).toBeVisible();
-      await alternateAddressPage.streetAddressInput.fill("476 5th Ave");
-      await alternateAddressPage.apartmentSuiteInput.fill("Room 200");
-      await alternateAddressPage.cityInput.fill("New York");
-      await alternateAddressPage.stateInput.fill("NY");
-      await alternateAddressPage.postalCodeInput.fill("10018");
-      await alternateAddressPage.nextButton.click();
-
-      const addressVerificationPage = new AddressVerificationPage(page);
-      await expect(
-        addressVerificationPage.alternateAddressHeader
-      ).toBeVisible();
-    });
-
-    test("navigates to Alternate Address page from Personal page", async ({
-      page,
-    }) => {
-      await page.goto("/library-card/personal?newCard=true");
-      const personalPage = new PersonalPage(page);
-      await expect(personalPage.stepHeading).toBeVisible();
-      await personalPage.firstNameInput.fill("Test");
-      await personalPage.lastNameInput.fill("User");
-      await personalPage.dateOfBirthInput.fill("01/01/1990");
-      await personalPage.emailInput.fill("test@example.com");
-      await personalPage.nextButton.click();
-
-      const addressPage = new AddressPage(page);
-      await expect(addressPage.addressHeading).toBeVisible();
-      await addressPage.streetAddressInput.fill("123 Main St");
-      await addressPage.cityInput.fill("New York");
-      await addressPage.stateInput.fill("NY");
-      await addressPage.postalCodeInput.fill("10001");
-      await addressPage.nextButton.click();
-
-      const alternateAddressPage = new AlternateAddressPage(page);
-      await expect(alternateAddressPage.addressHeading).toBeVisible();
-      await alternateAddressPage.streetAddressInput.fill("476 5th Ave");
-      await alternateAddressPage.apartmentSuiteInput.fill("Room 200");
-      await alternateAddressPage.cityInput.fill("New York");
-      await alternateAddressPage.stateInput.fill("NY");
-      await alternateAddressPage.postalCodeInput.fill("10018");
-      await alternateAddressPage.nextButton.click();
-
-      const addressVerificationPage = new AddressVerificationPage(page);
-      await expect(
-        addressVerificationPage.alternateAddressHeader
-      ).toBeVisible();
-    });
-
-    test("navigates to Alternate Address page from Home Address page", async ({
-      page,
-    }) => {
-      await page.goto("/library-card/location?newCard=true");
-      const addressPage = new AddressPage(page);
-      await expect(addressPage.addressHeading).toBeVisible();
-      await addressPage.streetAddressInput.fill("123 Main St");
-      await addressPage.cityInput.fill("New York");
-      await addressPage.stateInput.fill("NY");
-      await addressPage.postalCodeInput.fill("10001");
-      await addressPage.nextButton.click();
-
-      const alternateAddressPage = new AlternateAddressPage(page);
-      await expect(alternateAddressPage.addressHeading).toBeVisible();
-      await alternateAddressPage.streetAddressInput.fill("476 5th Ave");
-      await alternateAddressPage.apartmentSuiteInput.fill("Room 200");
-      await alternateAddressPage.cityInput.fill("New York");
-      await alternateAddressPage.stateInput.fill("NY");
-      await alternateAddressPage.postalCodeInput.fill("10018");
-      await alternateAddressPage.nextButton.click();
-
-      const addressVerificationPage = new AddressVerificationPage(page);
-      await expect(
-        addressVerificationPage.alternateAddressHeader
-      ).toBeVisible();
-    });
-  }
-);
