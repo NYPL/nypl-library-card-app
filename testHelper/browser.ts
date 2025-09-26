b69b2a8a136c8808947029279ee284bb5ca9cc33
toxiapo <toxiapo@gmail.com>
2025-09-26 15:15:19 -0400

    Set up baseline eslint with eslint9

--- testHelper/browser.ts
+++ testHelper/browser.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import "@testing-library/jest-dom";
 // Import this to be able to call this jest-axe assertion for all test files:
 // expect(...).toHaveNoViolations();
