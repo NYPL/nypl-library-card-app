b61eea0d393c1db838525332717087b7cbe9da83
Russell Cullen <russellcullen@nypl.org>
2025-09-29 13:54:17 -0400

    Remove unused policyType

--- src/components/AgeFormFields/index.tsx
+++ src/components/AgeFormFields/index.tsx
@@ -7,16 +7,12 @@ import FormField from "../FormField";
 import { isDate } from "../../utils/formDataUtils";
 import useFormDataContext from "../../context/FormDataContext";
 
-interface AgeFormProps {
-  policyType?: string;
-}
-
 /**
  * AgeForm
  * Renders an input field for "webApplicant" policy types and a checkbox for
  * "simplye" policy types.
  */
-const AgeForm = ({ policyType = "webApplicant" }: AgeFormProps) => {
+const AgeForm = () => {
   const { t } = useTranslation("common");
   const { state } = useFormDataContext();
   const { formValues } = state;
