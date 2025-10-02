59cb0b788c7d97e9836cec60ec6b6b3599341848
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-02 11:42:20 -0600

    Try entering personal info and jumping to review

--- playwright/tests/review.spec.ts
+++ playwright/tests/review.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from "@playwright/test";
+import { PersonalPage } from "../pageobjects/personal.page";
 import { ReviewPage } from "../pageobjects/review.page";
+import { TEST_PATRON_INFO } from "../utils/constants";
+import { fillPersonalInfo } from "../utils/form-helper";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/library-card/review?newCard=true");
-});
+test.describe("displays elements on Confirm Your Information page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/library-card/review?newCard=true");
+  });
 
-test("displays headings", async ({ page }) => {
-  const reviewPage = new ReviewPage(page);
-  await expect(reviewPage.mainHeading).toBeVisible();
-  await expect(reviewPage.stepHeading).toBeVisible();
-});
+  test("displays headings", async ({ page }) => {
+    const reviewPage = new ReviewPage(page);
+    await expect(reviewPage.mainHeading).toBeVisible();
+    await expect(reviewPage.stepHeading).toBeVisible();
+  });
 
-test.describe("displays personal information section", () => {
-  test("displays personal information headings", async ({ page }) => {
+  test("displays Personal Information section headings", async ({ page }) => {
     const reviewPage = new ReviewPage(page);
     await expect(reviewPage.personalInfoHeading).toBeVisible();
     await expect(reviewPage.firstNameHeading).toBeVisible();
@@ -21,9 +24,15 @@ test.describe("displays personal information section", () => {
     await expect(reviewPage.emailHeading).toBeVisible();
     await expect(reviewPage.receiveInfoHeading).toBeVisible();
   });
+});
 
-  test("displays patron information", async ({ page }) => {
-    const reviewPage = new ReviewPage(page);
-    await expect(reviewPage.receiveInfoChoice).toBeVisible();
-  });
+test("displays patron information", async ({ page }) => {
+  const personalPage = new PersonalPage(page);
+  await fillPersonalInfo(personalPage);
+  personalPage.nextButton.click();
+
+  await page.goto("/library-card/review?newCard=true");
+  const reviewPage = new ReviewPage(page);
+  // await expect(page.TEST_PATRON_INFO.firstName).toBeVisible();
+  await expect(reviewPage.receiveInfoChoice).toBeVisible();
 });
