be94649a0fb3d7d9f1940a4d87324e771cdc296f
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-02 11:11:31 -0600

    Update alternate address page with form helper function

--- playwright/utils/form-helper.ts
+++ playwright/utils/form-helper.ts
@@ -1,4 +1,8 @@
-import { TEST_PATRON_INFO } from "../utils/constants";
+import {
+  TEST_PATRON_INFO,
+  TEST_HOME_ADDRESS,
+  TEST_ALTERNATE_ADDRESS,
+} from "../utils/constants";
 
 export async function fillPersonalInfo(page) {
   await page.firstNameInput.fill(TEST_PATRON_INFO.firstName);
@@ -8,7 +12,7 @@ export async function fillPersonalInfo(page) {
   await page.checkBox.check();
 }
 
-// export async function fillAddress(page, address) {
+// export async function fillHomeAddress(page, address) {
 //   await page.fill('input[name="streetAddress"]', address.street);
 //   await page.fill('input[name="apartmentSuite"]', address.apartmentSuite);
 //   await page.fill('input[name="city"]', address.city);
@@ -16,10 +20,10 @@ export async function fillPersonalInfo(page) {
 //   await page.fill('input[name="postalCode"]', address.postalCode);
 // }
 
-// export async function fillAlternateAddress(page, address) {
-//   await page.fill('input[name="altStreetAddress"]', address.street);
-//   await page.fill('input[name="altApartmentSuite"]', address.apartmentSuite);
-//   await page.fill('input[name="altCity"]', address.city);
-//   await page.fill('input[name="altState"]', address.state);
-//   await page.fill('input[name="altPostalCode"]', address.postalCode);
-// }
+export async function fillAlternateAddress(page) {
+  await page.streetAddressInput.fill(TEST_ALTERNATE_ADDRESS.street);
+  await page.apartmentSuiteInput.fill(TEST_ALTERNATE_ADDRESS.apartmentSuite);
+  await page.cityInput.fill(TEST_ALTERNATE_ADDRESS.city);
+  await page.stateInput.fill(TEST_ALTERNATE_ADDRESS.state);
+  await page.postalCodeInput.fill(TEST_ALTERNATE_ADDRESS.postalCode);
+}
