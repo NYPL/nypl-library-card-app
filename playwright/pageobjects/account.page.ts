9cbc8154ad81d61527eb3e70503c7ea0637ead98
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-07 09:10:01 -0600

    Revert label yet again

--- playwright/pageobjects/account.page.ts
+++ playwright/pageobjects/account.page.ts
@@ -83,7 +83,7 @@ export class AccountPage {
     this.privacyPolicy = page
       .locator("#mainContent")
       .getByRole("link", { name: "Privacy Policy", exact: true });
-    this.acceptTermsCheckbox = page.getByLabel(
+    this.acceptTermsCheckbox = page.getByText(
       "Yes, I accept the terms and conditions.",
       { exact: true }
     );
