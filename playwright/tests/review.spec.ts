a5ce3855097f35c23f13640e7dc94d14dbd97fc0
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-08 15:48:59 -0600

    Move beforeEach and add test placeholders

--- playwright/tests/review.spec.ts
+++ playwright/tests/review.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { ReviewPage } from "../pageobjects/review.page";
 
-test.describe("displays elements on review page", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/library-card/review?newCard=true");
-  });
+test.beforeEach(async ({ page }) => {
+  await page.goto("/library-card/review?newCard=true");
+});
 
+test.describe("displays elements on review page", () => {
   test("displays headings", async ({ page }) => {
     const reviewPage = new ReviewPage(page);
     await expect(reviewPage.mainHeading).toBeVisible();
@@ -21,4 +21,9 @@ test.describe("displays elements on review page", () => {
     await expect(reviewPage.emailHeading).toBeVisible();
     await expect(reviewPage.receiveInfoHeading).toBeVisible();
   });
+
+  // test: displays home address section headings
+  // test: displays alternate address section headings
+  // test: displays create your account section headings
+  // test: displays submit button
 });
