24e8d7b2451893a3e55a21d63a9539d50e3e8137
roseb89 <dz4888@wayne.edu>
2025-10-10 11:35:51 -0400

    updated page manager class

--- playwright/pageobjects/page-manager.page.ts
+++ playwright/pageobjects/page-manager.page.ts
@@ -8,20 +8,32 @@ import { CongratsPage } from "./congrats.page";
 
 export class PageManager {
   readonly page: Page;
-  readonly landingPage: LandingPage;
-  readonly personalPage: PersonalPage;
-  readonly accountPage: AccountPage;
-  readonly addressPage: AddressPage;
-  readonly alternateAddressPage: AlternateAddressPage;
-  readonly congratsPage: CongratsPage;
 
   constructor(page: Page) {
     this.page = page;
-    this.landingPage = new LandingPage(page);
-    this.accountPage = new AccountPage(page);
-    this.addressPage = new AddressPage(page);
-    this.alternateAddressPage = new AlternateAddressPage(page);
-    this.personalPage = new PersonalPage(page);
-    this.congratsPage = new CongratsPage(page);
+  }
+
+  get landingPage(): LandingPage {
+    return new LandingPage(this.page);
+  }
+
+  get personalPage(): PersonalPage {
+    return new PersonalPage(this.page);
+  }
+
+  get addressPage(): AddressPage {
+    return new AddressPage(this.page);
+  }
+
+  get alternateAddressPage(): AlternateAddressPage {
+    return new AlternateAddressPage(this.page);
+  }
+
+  get accountPage(): AccountPage {
+    return new AccountPage(this.page);
+  }
+
+  get congratsPage(): CongratsPage {
+    return new CongratsPage(this.page);
   }
 }
