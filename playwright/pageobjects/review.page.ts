c65e7a5f2d9b84a96f68d2e438fc4f39607a05bb
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-10 10:11:20 -0600

    Removed locators

--- playwright/pageobjects/review.page.ts
+++ playwright/pageobjects/review.page.ts
@@ -1,5 +1,4 @@
 import { Page, Locator } from "@playwright/test";
-import { TEST_PATRON_INFO } from "../utils/constants";
 
 export class ReviewPage {
   readonly page: Page;
@@ -7,15 +6,10 @@ export class ReviewPage {
   readonly stepHeading: Locator;
   readonly personalInfoHeading: Locator;
   readonly firstNameHeading: Locator;
-  readonly firstNameValue: Locator;
   readonly lastNameHeading: Locator;
-  readonly lastNameValue: Locator;
   readonly dateOfBirthHeading: Locator;
-  readonly dateOfBirthValue: Locator;
   readonly emailHeading: Locator;
-  readonly emailValue: Locator;
   readonly receiveInfoHeading: Locator;
-  readonly receiveInfoChoice: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -32,23 +26,12 @@ export class ReviewPage {
       level: 3,
     });
     this.firstNameHeading = page.getByText("First Name", { exact: true });
-    this.firstNameValue = page.getByText(TEST_PATRON_INFO.firstName, {
-      exact: true,
-    });
     this.lastNameHeading = page.getByText("Last Name", { exact: true });
-    this.lastNameValue = page.getByText(TEST_PATRON_INFO.lastName, {
-      exact: true,
-    });
     this.dateOfBirthHeading = page.getByText("Date of Birth", { exact: true });
-    this.dateOfBirthValue = page.getByText(TEST_PATRON_INFO.dateOfBirth, {
-      exact: true,
-    });
     this.emailHeading = page.getByText("Email Address", { exact: true });
-    this.emailValue = page.getByText(TEST_PATRON_INFO.email, { exact: true });
     this.receiveInfoHeading = page.getByText(
       "Receive information about NYPL's programs and services",
       { exact: true }
     );
-    this.receiveInfoChoice = page.getByText("Yes", { exact: true });
   }
 }
