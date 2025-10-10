c1eeb8d8d0d3444fb18623cc265f49896cc8b8c6
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2025-10-10 12:53:16 -0600

    SWIS-100: Playwright test to confirm Personal Information headings on Step 5: Confirm Your Information page (#410)
    * Add initial test to confirm a few headings
    
    * Add test to display personal info heading
    
    * Update personal info page with form helper function
    
    * Update alternate address page with form helper function
    
    * Update home address page with form helper function
    
    * Update address verification page with form helper function
    
    * Try entering personal info and jumping to review
    
    * Missing goto
    
    * Add flow to confirm patron personal info displays
    
    * Add remaining personal info values
    
    * Update changelog
    
    * Update changelog
    
    * Fix changelog
    
    * Address copilot feedback
    
    * Add awaits
    
    * Retry checkbox role
    
    * Revert checkbox to text
    
    * Try 30s timeout
    
    * Updated most page types
    
    * Add interfaces
    
    * Move interfaces to new types.ts
    
    * Update address const with type
    
    * Try label for checkbox
    
    * Remove timeout for now
    
    * Split test into steps
    
    * Revert checkbox locator to text
    
    * Try exact label one more time
    
    * Ensure test is used once
    
    * Revert label yet again
    
    * Update assertions to toHaveText
    
    * Removed tests
    
    * Removed locators
    
    * Move beforeEach

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@
 - Add Playwright test to confirm user input on home address page [SWIS-89](https://newyorkpubliclibrary.atlassian.net/browse/SWIS-89)
 - Enable eslint 9 [SWIS-94](https://newyorkpubliclibrary.atlassian.net/browse/SWIS-94)
 - Add Playwright util to fill in forms [SWIS-115](https://newyorkpubliclibrary.atlassian.net/browse/SWIS-115)
+- Add Playwright test to confirm personal information on review page [SWIS-100](https://newyorkpubliclibrary.atlassian.net/browse/SWIS-100)
 
 ### 1.2.6
 
---
+++ playwright/tests/review.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+import { ReviewPage } from "../pageobjects/review.page";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/library-card/review?newCard=true");
+});
+test.describe("displays elements on review page", () => {
+  test("displays headings", async ({ page }) => {
+    const reviewPage = new ReviewPage(page);
+    await expect(reviewPage.mainHeading).toBeVisible();
+    await expect(reviewPage.stepHeading).toBeVisible();
+  });
+
+  test("displays Personal Information section headings", async ({ page }) => {
+    const reviewPage = new ReviewPage(page);
+    await expect(reviewPage.personalInfoHeading).toBeVisible();
+    await expect(reviewPage.firstNameHeading).toBeVisible();
+    await expect(reviewPage.lastNameHeading).toBeVisible();
+    await expect(reviewPage.dateOfBirthHeading).toBeVisible();
+    await expect(reviewPage.emailHeading).toBeVisible();
+    await expect(reviewPage.receiveInfoHeading).toBeVisible();
+  });
+});
