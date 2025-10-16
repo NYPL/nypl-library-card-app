e9d75bb7a42aa07d50dad81d49ecf3aa3f9f4e5e
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-10-16 11:04:50 -0600

    Add describe block

--- playwright/tests/congrats.spec.ts
+++ playwright/tests/congrats.spec.ts
@@ -5,27 +5,29 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/library-card/congrats?&newCard=true");
 });
 
-test("displays headings on Congrats page", async ({ page }) => {
-  const congratsPage = new CongratsPage(page);
-  await expect(congratsPage.mainHeading).toBeVisible();
-  await expect(congratsPage.stepHeading).toBeVisible();
-  await expect(congratsPage.getStartedHeading).toBeVisible();
-});
+test.describe("displays elements on Congrats page", () => {
+  test("displays headings on Congrats page", async ({ page }) => {
+    const congratsPage = new CongratsPage(page);
+    await expect(congratsPage.mainHeading).toBeVisible();
+    await expect(congratsPage.stepHeading).toBeVisible();
+    await expect(congratsPage.getStartedHeading).toBeVisible();
+  });
 
-test("displays library card on Congrats page", async ({ page }) => {
-  const congratsPage = new CongratsPage(page);
-  await expect(congratsPage.memberNameHeading).toBeVisible();
-  await expect(congratsPage.issuedDateHeading).toBeVisible();
-  await expect(congratsPage.issuedDate).toBeVisible();
-  await expect(congratsPage.libraryCardBackground).toBeVisible();
-});
+  test("displays library card on Congrats page", async ({ page }) => {
+    const congratsPage = new CongratsPage(page);
+    await expect(congratsPage.memberNameHeading).toBeVisible();
+    await expect(congratsPage.issuedDateHeading).toBeVisible();
+    await expect(congratsPage.issuedDate).toBeVisible();
+    await expect(congratsPage.libraryCardBackground).toBeVisible();
+  });
 
-test("displays links on Congrats page", async ({ page }) => {
-  const congratsPage = new CongratsPage(page);
-  await expect(congratsPage.locationsLink).toBeVisible();
-  await expect(congratsPage.photoIdAndProofOfAddressLink).toBeVisible();
-  await expect(congratsPage.getHelpEmailLink).toBeVisible();
-  await expect(congratsPage.loginLink).toBeVisible();
-  await expect(congratsPage.findOutLibraryLink).toBeVisible();
-  await expect(congratsPage.discoverLink).toBeVisible();
+  test("displays links on Congrats page", async ({ page }) => {
+    const congratsPage = new CongratsPage(page);
+    await expect(congratsPage.locationsLink).toBeVisible();
+    await expect(congratsPage.photoIdAndProofOfAddressLink).toBeVisible();
+    await expect(congratsPage.getHelpEmailLink).toBeVisible();
+    await expect(congratsPage.loginLink).toBeVisible();
+    await expect(congratsPage.findOutLibraryLink).toBeVisible();
+    await expect(congratsPage.discoverLink).toBeVisible();
+  });
 });
