d605f8c7ca146db1516a74d1420730f96d30396a
roseb89 <dz4888@wayne.edu>
2025-10-28 10:31:43 -0400

    udpated test

---
+++ playwright/api_tests/sierra-api-tests.spec.ts
@@ -0,0 +1,13 @@
+/*import { test, expect } from "@playwright/test";
+
+
+test.use({
+
+     baseURL: "https://nypl-sierra-test.nypl.org",
+        extraHTTPHeaders:{
+          'Authorization': `Basic ${process.env.SIERRA_BASIC_AUTH_BASE64}`,
+          'Content-Type': "application/x-www-form-urlencoded",
+        }
+})
+
+test.beforeEach(async({}))*/
