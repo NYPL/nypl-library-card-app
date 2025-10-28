6f7dde3c7d01ffe38717b6219cc083408ef58438
roseb89 <dz4888@wayne.edu>
2025-10-28 10:35:45 -0400

    updated test

--- playwright/api_tests/sierra-api-tests.spec.ts
+++ playwright/api_tests/sierra-api-tests.spec.ts
@@ -4,10 +4,16 @@
 test.use({
 
      baseURL: "https://nypl-sierra-test.nypl.org",
-        extraHTTPHeaders:{
+     
+})
+
+test.beforeEach(async({}))
+   
+   await request.fetch("/iii/sierra-api/v6/token")
+   method: 'post'
+   extraHTTPHeaders:{
           'Authorization': `Basic ${process.env.SIERRA_BASIC_AUTH_BASE64}`,
           'Content-Type': "application/x-www-form-urlencoded",
         }
-})
 
-test.beforeEach(async({}))*/
+*/
