9cce13f907769f8c9d8ff83a27b6532a316e310d
Russell Cullen <russellcullen@nypl.org>
2025-11-06 10:04:17 -0500

    Factor styles out

--- src/components/ConfirmationGraphic/index.tsx
+++ src/components/ConfirmationGraphic/index.tsx
@@ -12,6 +12,78 @@ import { FormResults } from "../../interfaces";
 import useFormDataContext from "../../context/FormDataContext";
 import { useTranslation } from "next-i18next";
 
+const styles = {
+  outerBox: {
+    mb: "s",
+    bg: "var(--nypl-colors-ui-gray-light-cool)",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  imageLion: {
+    position: "relative",
+    fontSize: "1em",
+    mx: "0 auto",
+    width: { base: "100%", md: "80%", lg: "60%" },
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  backgroundLion: {
+    top: "6",
+    width: "100%",
+    position: "absolute",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    p: "0 40px 0 25px",
+    gap: "10px 5px",
+    gridTemplateColumns: "58% 30%",
+    gridTemplateRows: "20% 40% 20%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  memberName: {
+    color: "white",
+    fontSize: "0.8em",
+  },
+  logoItem: {
+    color: "white",
+    fontSize: "0.6em",
+    mt: "20px",
+  },
+  barcodeContainer: {
+    bg: "white",
+    p: "15px 20px 0px",
+    display: "inline-block",
+    gridColumn: "1/3",
+    gridRow: "2/2",
+    borderTopLeftRadius: "3px",
+    borderTopRightRadius: "3px",
+    borderBottomLeftRadius: "5px",
+    borderBottomRightRadius: "5px",
+  },
+  barcode: {
+    color: "black",
+    mx: "auto",
+    position: "relative",
+    display: "table",
+    fontSize: {
+      base: "1.4em",
+      sm: "1.4em",
+      lg: "1.9em",
+    },
+  },
+  issuedText: {
+    color: "white",
+    fontSize: "1em",
+    mb: "s",
+  },
+};
+
 const ConfirmationContainer = () => {
   const { state } = useFormDataContext();
   const formResults = state.results || ({} as FormResults);
@@ -46,86 +118,38 @@ const ConfirmationContainer = () => {
   }, [canvas]);
 
   return (
-    <Box
-      mb="s"
-      bg="var(--nypl-colors-ui-gray-light-cool)"
-      position="relative"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      width="100%"
-    >
-      <Box
-        className="image-lion"
-        position="relative"
-        fontSize="1em"
-        mx="0 auto"
-        width={{ base: "100%", md: "80%", lg: "60%" }}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-      >
+    <Box sx={styles.outerBox}>
+      <Box className="image-lion" sx={styles.imageLion}>
         <Image
           alt="NYPL Library Barcode Background"
           height="727"
           src="/library-card/cardbg.png"
           width="939"
         />
-        <Grid
-          className="background-lion"
-          top="6"
-          width="100%"
-          position="absolute"
-          backgroundSize="cover"
-          backgroundRepeat="no-repeat"
-          height="100%"
-          p="0 40px 0 25px"
-          gap="10px 5px"
-          gridTemplateColumns="58% 30%"
-          gridTemplateRows="20% 40% 20%"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <GridItem id="member-name" color="white" fontSize="0.8em">
+        <Grid className="background-lion" sx={styles.backgroundLion}>
+          <GridItem id="member-name" sx={styles.memberName}>
             {t("confirmation.graphic.memberName")}
             <Box className="content" fontSize="1.6em">
               {name}
             </Box>
           </GridItem>
-          <GridItem color="white" fontSize="0.6em" mt="20px">
+          <GridItem sx={styles.logoItem}>
             <Logo decorative name="nyplFullWhite" size="small" mb="xs" />
           </GridItem>
           <Box
             className="grid-item barcode-container"
-            bg="white"
-            p="15px 20px 0px"
-            display="inline-block"
-            gridColumn="1/3"
-            gridRow="2/2"
-            borderTopLeftRadius="3px"
-            borderTopRightRadius="3px"
-            borderBottomLeftRadius="5px"
-            borderBottomRightRadius="5px"
+            sx={styles.barcodeContainer}
           >
             <canvas
               id="barcodeCanvas"
               {...canvasArgs}
               style={{ width: "100%", height: "50px" }}
             ></canvas>
-            <Box
-              className="barcode"
-              color="black"
-              mx="auto"
-              position="relative"
-              fontSize="2em"
-              display="table"
-            >
+            <Box className="barcode" sx={styles.barcode}>
               {barcode}
             </Box>
           </Box>
-
-          <GridItem id="issued" color="white" fontSize="1em" mb="s">
+          <GridItem id="issued" sx={styles.issuedText}>
             {t("confirmation.graphic.issued")}
             <Box className="content" fontSize={"1.6em"}>
               {new Date().toLocaleDateString()}
