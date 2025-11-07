d86a20a559d7f11d2db502c96c898478ded8e068
Russell Cullen <russellcullen@nypl.org>
2025-11-07 16:45:18 -0500

    Add scroll to page headers on next step

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useRef } from "react";
 import { Heading } from "@nypl/design-system-react-components";
 
 /**
  * PageHeading component that's used for the main heading component on each page
  */
 export const PageHeading = ({ children, ...rest }) => {
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    // Delay scroll to ensure new content is rendered before scrolling
+    const timer = setTimeout(() => {
+      if (headingRef.current) {
+        headingRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Heading level="h2" mb="s" size="heading3" {...rest}>
+    <Heading
+      ref={headingRef}
+      tabIndex={-1}
+      level="h2"
+      mb="s"
+      size="heading3"
+      {...rest}
+    >
       {children}
     </Heading>
   );
