a3ee9f1950ed8a80f7c206a08e323c379966f749
toxiapo <toxiapo@gmail.com>
2025-11-13 16:16:09 -0500

    don't log to file

--- newrelic.js
+++ newrelic.js
@@ -21,6 +21,10 @@ exports.config = {
      * production applications.
      */
     level: "info",
+    /**
+     * Stops NR from creating a newrelic.log file
+     */
+    filepath: "stdout",
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
