cc94ef0cebe61c9fdb99f09444fc9a11f17330bb
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2025-12-05 15:53:15 -0700

    Revert home library back to code

--- playwright/utils/constants.ts
+++ playwright/utils/constants.ts
@@ -20,7 +20,7 @@ export const TEST_HOME_ADDRESS: AddressData = {
 export const TEST_CUSTOMIZE_ACCOUNT = {
   username: UNIQUE_USERNAME,
   password: "Test@1234",
-  homeLibrary: "E-Branch",
+  homeLibrary: "eb",
 };
 export const TEST_ALTERNATE_ADDRESS: AddressData = {
   street: "476 5th Ave",
