e6a8aa30ae7dccf06df4e5217ae574ba1d7ab8fd
Bridgette Rose <dz4888@wayne.edu>
2025-12-19 16:05:16 -0500

    Accessibility CI setup (GitHub Actions) (#479)
    * Accessibility CI setup (GitHub Actions)
    
    * update accessibility yml and remove stringify from text
    
    * update accessibility yml and remove stringify from text
    
    * add locators to .include() to be analyzed
    
    * scan whole page landing page
    
    * updated readme
    
    * add tests for keyboard navigation and to check accessible names, update readme
    
    * removed launch options form webkit in config, update keyboard navigation test to have array, update readme
    
    * fix keyboard navigation test to tab each link individually
    
    * fix keyboard navigation test to tab each link individually, removed used variable
    
    * removed networkirdle, removed installation from readme
    
    * add back the array for language keyboard navigation
    
    * removed tabKey and  using Tab
    
    * update the readme to include guidelines, removed toHaveAccessibleName() test and updated keyboard navigation test
    
    * added if to for loop
    
    * updated the accessibility section in the readme and for loop

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,9 @@ export default defineConfig({
 
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      use: {
+        ...devices["Desktop Safari"],
+      },
     },
 
     /* Test against mobile viewports. */
---
+++ playwright/tests/accessibility-tests/landing-a11y.spec.ts
@@ -0,0 +1,43 @@
+import { LandingPage } from "../../pageobjects/landing.page";
+import { test, expect } from "@playwright/test";
+import { AxeBuilder } from "@axe-core/playwright";
+
+test.describe("Accessibility tests on Landing Page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/library-card/new");
+  });
+
+  test("should have no accessibility violations on load", async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag21aa", "wcag22aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toHaveLength(0);
+  });
+
+  test("should have keyboard focus indicators for language links", async ({
+    page,
+  }) => {
+    const landingPage = new LandingPage(page);
+
+    const languageLocators = [
+      landingPage.arabicLanguage,
+      landingPage.bengaliLanguage,
+      landingPage.chineseLanguage,
+      landingPage.englishLanguage,
+      landingPage.frenchLanguage,
+      landingPage.haitianCreoleLanguage,
+      landingPage.koreanLanguage,
+      landingPage.polishLanguage,
+      landingPage.russianLanguage,
+      landingPage.spanishLanguage,
+      landingPage.urduLanguage,
+    ];
+
+    await languageLocators[0].focus();
+    await expect(languageLocators[0]).toBeFocused();
+    for (let i = 1; i < languageLocators.length; i++) {
+      await page.keyboard.press("Tab");
+      await expect(languageLocators[i]).toBeFocused();
+    }
+  });
+});
