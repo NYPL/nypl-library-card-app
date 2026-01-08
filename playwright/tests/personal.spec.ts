e4d3fb4c0800bfdd34c0dfca0381334ac951d596
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-01-08 08:22:08 -0700

    SWIS-235: Clean up Playwright tests on `personal` page (#487)
    * Group displays elements tests
    
    * Reorganize and rename tests
    
    * Remove test
    
    * Rearrange DOB and email
    
    * Rearrange DOB and email in form helper
    
    * Rearrange tests
    
    * Add top-level domain email test

--- playwright/tests/personal.spec.ts
+++ playwright/tests/personal.spec.ts
@@ -7,81 +7,94 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/library-card/personal?newCard=true");
 });
 
-test("Display personal information form", async ({ page }) => {
-  const personalPage = new PersonalPage(page);
-  await expect(personalPage.mainHeading).toBeVisible();
-  await expect(personalPage.stepHeading).toBeVisible();
-  await expect(personalPage.firstNameInput).toBeVisible();
-  await expect(personalPage.lastNameInput).toBeVisible();
-  await expect(personalPage.emailInput).toBeVisible();
-  await expect(personalPage.dateOfBirthInput).toBeVisible();
-  await expect(personalPage.receiveInfoCheckbox).toBeVisible();
-  await expect(personalPage.alternateFormLink).toBeVisible();
-  await expect(personalPage.locationsLink).toBeVisible();
-  await expect(personalPage.previousButton).toBeVisible();
-  await expect(personalPage.nextButton).toBeVisible();
+test.describe("displays elements on personal information page", () => {
+  test("displays headings", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await expect(personalPage.mainHeading).toBeVisible();
+    await expect(personalPage.stepHeading).toBeVisible();
+  });
+
+  test("displays personal information form", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await expect(personalPage.firstNameInput).toBeVisible();
+    await expect(personalPage.lastNameInput).toBeVisible();
+    await expect(personalPage.dateOfBirthInput).toBeVisible();
+    await expect(personalPage.emailInput).toBeVisible();
+    await expect(personalPage.receiveInfoCheckbox).toBeVisible();
+  });
+
+  test("displays links", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await expect(personalPage.alternateFormLink).toBeVisible();
+    await expect(personalPage.locationsLink).toBeVisible();
+  });
+
+  test("displays next and previous buttons", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await expect(personalPage.previousButton).toBeVisible();
+    await expect(personalPage.nextButton).toBeVisible();
+  });
 });
 
-test("error validation for empty personal info form", async ({ page }) => {
-  const personalPage = new PersonalPage(page);
-  await personalPage.firstNameInput.fill("");
-  await personalPage.lastNameInput.fill("");
-  await personalPage.emailInput.fill("");
-  await personalPage.dateOfBirthInput.fill("");
-
-  await personalPage.nextButton.click();
-
-  await expect(personalPage.firstNameErrorMessage).toBeVisible();
-  await expect(personalPage.lastNameErrorMessage).toBeVisible();
-  await expect(personalPage.emailErrorMessage).toBeVisible();
-  await expect(personalPage.dateOfBirthErrorMessage).toBeVisible();
+test.describe("enters personal information", () => {
+  test("enters valid personal information", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await fillPersonalInfo(personalPage);
+    await expect(personalPage.firstNameInput).toHaveValue(
+      TEST_PATRON_INFO.firstName
+    );
+    await expect(personalPage.lastNameInput).toHaveValue(
+      TEST_PATRON_INFO.lastName
+    );
+    await expect(personalPage.dateOfBirthInput).toHaveValue(
+      TEST_PATRON_INFO.dateOfBirth
+    );
+    await expect(personalPage.emailInput).toHaveValue(TEST_PATRON_INFO.email);
+  });
 });
 
-test("error validation for invalid email format", async ({ page }) => {
-  const personalPage = new PersonalPage(page);
-  await personalPage.emailInput.fill("testgmail.com");
-  await personalPage.nextButton.click();
-  await expect(personalPage.emailErrorMessage).toBeVisible();
-});
+test.describe("displays error messages", () => {
+  test("displays errors for required fields", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await personalPage.firstNameInput.fill("");
+    await personalPage.lastNameInput.fill("");
+    await personalPage.dateOfBirthInput.fill("");
+    await personalPage.emailInput.fill("");
+    await personalPage.nextButton.click();
 
-test("error validation for missing email domain", async ({ page }) => {
-  const personalPage = new PersonalPage(page);
-  await personalPage.emailInput.fill("test@.com");
-  await personalPage.nextButton.click();
-  await expect(personalPage.emailErrorMessage).toBeVisible();
-});
+    await expect(personalPage.firstNameErrorMessage).toBeVisible();
+    await expect(personalPage.lastNameErrorMessage).toBeVisible();
+    await expect(personalPage.dateOfBirthErrorMessage).toBeVisible();
+    await expect(personalPage.emailErrorMessage).toBeVisible();
+  });
 
-test("error validation for missing email username", async ({ page }) => {
-  const personalPage = new PersonalPage(page);
-  await personalPage.emailInput.fill("@gmail.com");
-  await personalPage.nextButton.click();
-  await expect(personalPage.emailErrorMessage).toBeVisible();
-});
+  test("displays error for missing email symbol", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await personalPage.emailInput.fill("testgmail.com");
+    await personalPage.nextButton.click();
+    await expect(personalPage.emailErrorMessage).toBeVisible();
+  });
 
-test("error validation for birth date less than 13 years ago", async ({
-  page,
-}) => {
-  const personalPage = new PersonalPage(page);
-  const today = new Date();
-  const minDate = new Date(today.setFullYear(today.getFullYear() - 13));
-  await personalPage.dateOfBirthInput.fill(minDate.toISOString().split("T")[0]);
-  await personalPage.nextButton.click();
-  await expect(personalPage.dateOfBirthErrorMessage).toBeVisible();
-});
+  test("displays error for missing email domain", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await personalPage.emailInput.fill("test@.com");
+    await personalPage.nextButton.click();
+    await expect(personalPage.emailErrorMessage).toBeVisible();
+  });
 
-test("input patron's personal info into personal information form", async ({
-  page,
-}) => {
-  const personalPage = new PersonalPage(page);
-  await fillPersonalInfo(personalPage);
-  await expect(personalPage.firstNameInput).toHaveValue(
-    TEST_PATRON_INFO.firstName
-  );
-  await expect(personalPage.lastNameInput).toHaveValue(
-    TEST_PATRON_INFO.lastName
-  );
-  await expect(personalPage.emailInput).toHaveValue(TEST_PATRON_INFO.email);
-  await expect(personalPage.dateOfBirthInput).toHaveValue(
-    TEST_PATRON_INFO.dateOfBirth
-  );
+  test("displays error for missing email username", async ({ page }) => {
+    const personalPage = new PersonalPage(page);
+    await personalPage.emailInput.fill("@gmail.com");
+    await personalPage.nextButton.click();
+    await expect(personalPage.emailErrorMessage).toBeVisible();
+  });
+
+  test("displays error for missing email top-level domain", async ({
+    page,
+  }) => {
+    const personalPage = new PersonalPage(page);
+    await personalPage.emailInput.fill("user@gmail");
+    await personalPage.nextButton.click();
+    await expect(personalPage.emailErrorMessage).toBeVisible();
+  });
 });
