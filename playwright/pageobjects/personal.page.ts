6b883b6653ad0a774f87ea0c52e7eae941f92886
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-01-15 11:46:47 -0700

    Rename and organize personal locators

--- playwright/pageobjects/personal.page.ts
+++ playwright/pageobjects/personal.page.ts
@@ -6,18 +6,18 @@ export class PersonalPage {
   readonly mainHeading: Locator; // displays on each page
   readonly stepHeading: Locator;
   readonly firstNameInput: Locator;
+  readonly firstNameError: Locator;
   readonly lastNameInput: Locator;
-  readonly emailInput: Locator;
+  readonly lastNameError: Locator;
   readonly dateOfBirthInput: Locator;
+  readonly dateOfBirthError: Locator;
+  readonly emailInput: Locator;
+  readonly emailError: Locator;
+  readonly alternateFormLink: Locator;
+  readonly locationsLink: Locator;
   readonly receiveInfoCheckbox: Locator;
   readonly previousButton: Locator;
   readonly nextButton: Locator;
-  readonly firstNameErrorMessage: Locator;
-  readonly lastNameErrorMessage: Locator;
-  readonly emailErrorMessage: Locator;
-  readonly dateOfBirthErrorMessage: Locator;
-  readonly alternateFormLink: Locator;
-  readonly locationsLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -30,9 +30,25 @@ export class PersonalPage {
       level: 2,
     });
     this.firstNameInput = this.page.getByLabel(/First name/i);
+    this.firstNameError = this.page.getByText(
+      ERROR_MESSAGES.FIRST_NAME_INVALID
+    );
     this.lastNameInput = this.page.getByLabel(/Last name/i);
+    this.lastNameError = this.page.getByText(ERROR_MESSAGES.LAST_NAME_INVALID);
     this.dateOfBirthInput = this.page.getByLabel(/Date of birth/i);
+    this.dateOfBirthError = this.page.getByText(
+      ERROR_MESSAGES.DATE_OF_BIRTH_INVALID
+    );
     this.emailInput = this.page.getByLabel(/Email/i);
+    this.emailError = this.page.getByText(ERROR_MESSAGES.EMAIL_INVALID);
+    this.alternateFormLink = this.page.getByRole("link", {
+      name: "alternate form",
+      exact: true,
+    });
+    this.locationsLink = this.page.getByRole("link", {
+      name: "locations",
+      exact: true,
+    });
     this.receiveInfoCheckbox = this.page.getByText(
       "Yes, I would like to receive information about NYPL's programs and services"
     );
@@ -42,26 +58,6 @@ export class PersonalPage {
     });
     this.nextButton = this.page.getByRole("button", {
       name: "Next",
-      exact: true,
-    });
-
-    this.firstNameErrorMessage = this.page.getByText(
-      ERROR_MESSAGES.FIRST_NAME_INVALID
-    );
-    this.lastNameErrorMessage = this.page.getByText(
-      ERROR_MESSAGES.LAST_NAME_INVALID
-    );
-    this.emailErrorMessage = this.page.getByText(ERROR_MESSAGES.EMAIL_INVALID);
-    this.dateOfBirthErrorMessage = this.page.getByText(
-      ERROR_MESSAGES.DATE_OF_BIRTH_INVALID
-    );
-
-    this.alternateFormLink = this.page.getByRole("link", {
-      name: "alternate form",
-      exact: true,
-    });
-    this.locationsLink = this.page.getByRole("link", {
-      name: "locations",
       exact: true,
     });
   }
