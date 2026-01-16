64cca9cad4a4bfdc9065e9eb150b68accc7be11f
Bridgette Rose <dz4888@wayne.edu>
2026-01-16 12:50:57 -0500

    address a11y tests (#493)
    * address a11y tests
    
    * use array
    
    * update name, focus , tab
    
    * renamed test,  updated function
    
    * updated tests
    
    * removed previous and next buttons
    
    * removed if statement
    
    * add tobeFocused() to step heading
    
    * removed focus() for step heading.

---
+++ playwright/tests/accessibility-tests/address-a11y.spec.ts
@@ -0,0 +1,34 @@
+import { AddressPage } from "../../pageobjects/address.page";
+import { test, expect } from "@playwright/test";
+import { AxeBuilder } from "@axe-core/playwright";
+
+test.describe("Accessibility tests on Address Page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/library-card/location?newCard=true");
+  });
+
+  test("should have no accessibility violations on load", async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag21aa", "wcag22aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toHaveLength(0);
+  });
+  test("should reach all form fields via the tab key", async ({ page }) => {
+    const addressPage = new AddressPage(page);
+
+    const addressLocators = [
+      addressPage.streetAddressInput,
+      addressPage.apartmentSuiteInput,
+      addressPage.cityInput,
+      addressPage.stateInput,
+      addressPage.postalCodeInput,
+    ];
+
+    await expect(addressPage.stepHeading).toBeFocused();
+
+    for (const locator of addressLocators) {
+      await page.keyboard.press("Tab");
+      await expect(locator).toBeFocused();
+    }
+  });
+});
