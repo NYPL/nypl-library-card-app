ae35c6b6263a37c81d10c44aeab1bd38d0e9ec89
toxiapo <toxiapo@gmail.com>
2026-01-21 14:33:15 -0500

    update home link

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,9 +1,16 @@
 import { Breadcrumbs as DSBreadcrumbs } from "@nypl/design-system-react-components";
 import Link from "next/link";
 
+const qaHref = "https://qa-www.nypl.org";
+const prodHref = "https://www.nypl.org";
 const Breadcrumbs = () => {
+  const homeHref =
+    typeof window !== "undefined" && window.location.hostname === "www.nypl.org"
+      ? prodHref
+      : qaHref;
   const breadcrumbsData = [
-    { url: "https://www.nypl.org", text: "Home", replace: false },
+    // "/" redirects to "/library-card" url so we have to hardcode this here.
+    { url: homeHref, text: "Home", replace: false },
     { url: "/new", text: "Get A Library Card", replace: true },
   ];
   return (
