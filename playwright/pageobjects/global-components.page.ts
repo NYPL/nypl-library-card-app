9f68fd77d7ff27d11213509cf5053e6963ee48fa
roseb89 <dz4888@wayne.edu>
2026-01-23 11:58:23 -0500

    removed breadcrumb locator

--- playwright/pageobjects/global-components.page.ts
+++ playwright/pageobjects/global-components.page.ts
@@ -4,11 +4,9 @@ export class GlobalComponentsPage {
   readonly page: Page;
   readonly homeBreadcrumb: Locator;
   readonly getLibraryCardBreadcrumb: Locator;
-  readonly breadcrumb: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.breadcrumb = page.getByRole("navigation", { name: "Breadcrumb" });
     this.homeBreadcrumb = page.getByRole("link", { name: "Home", exact: true });
     this.getLibraryCardBreadcrumb = page
       .getByLabel("Breadcrumb")
