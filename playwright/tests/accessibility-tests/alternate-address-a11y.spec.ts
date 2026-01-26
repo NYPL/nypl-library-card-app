010e3f3cf9b37cf10bf5f429facc646b0898498b
roseb89 <dz4888@wayne.edu>
2026-01-26 17:08:29 -0500

    alternate address a11y tests

---
+++ playwright/tests/accessibility-tests/alternate-address-a11y.spec.ts
@@ -0,0 +1,34 @@
+import AxeBuilder from "@axe-core/playwright";
+import { AlternateAddressPage } from "../../pageobjects/alternate-address.page";
+import { test, expect } from "@playwright/test";
+
+test.describe("Accessibility tests on Alternate Address page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/library-card/workAddress?&newCard=true");
+  });
+
+  test("should have no accessibility violations on load", async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag21aa", "wcag22aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toHaveLength(0);
+  });
+
+  test("should reach all form fields via the tab key", async ({ page }) => {
+    const alternateAddress = new AlternateAddressPage(page);
+
+    const alternateAddressLocators = [
+      alternateAddress.streetAddressInput,
+      alternateAddress.apartmentSuiteInput,
+      alternateAddress.cityInput,
+      alternateAddress.stateInput,
+      alternateAddress.postalCodeInput,
+    ];
+    await expect(alternateAddress.stepHeading).toBeFocused();
+
+    for (const locator of alternateAddressLocators) {
+      await page.keyboard.press("Tab");
+      await expect(locator).toBeFocused();
+    }
+  });
+});
