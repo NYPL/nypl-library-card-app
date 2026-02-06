1fb5d724d0d158e388e79abe5f9187fca8a569e0
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-02-06 13:26:29 -0700

    Remove test

--- playwright/tests/address.spec.ts
+++ playwright/tests/address.spec.ts
@@ -81,13 +81,6 @@ test.describe("displays error messages", () => {
     await expect(addressPage.postalCodeError).toBeVisible();
   });
 
-  test("enter invalid postal code", async ({ page }) => {
-    const addressPage = new AddressPage(page);
-    await addressPage.postalCodeInput.fill("ABCDE");
-    await addressPage.nextButton.click();
-    await expect(addressPage.postalCodeError).toBeVisible();
-  });
-
   test("enter postal code with dash", async ({ page }) => {
     const addressPage = new AddressPage(page);
     await addressPage.postalCodeInput.fill("12345-6789");
