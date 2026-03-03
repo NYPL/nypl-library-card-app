ae4e2743cb26f5df5aab1e0a305bbff1cf2ded0a
roseb89 <dz4888@wayne.edu>
2026-03-03 08:01:12 -0500

    ny state mock

--- playwright/utils/mock-api.ts
+++ playwright/utils/mock-api.ts
@@ -13,13 +13,42 @@ export async function mockUsernameApi(page: Page, message: string) {
 export async function mockCreatePatronApi(
   page: Page,
   name: string,
-  barcode: string
+  barcode: string,
+  ptype: number
 ) {
   await page.route("**/library-card/api/create-patron", async (route) => {
     await route.fulfill({
       status: 200,
       contentType: "application/json",
-      body: JSON.stringify({ name, barcode }),
+      body: JSON.stringify({ name, barcode, ptype: ptype == 9 }),
+    });
+  });
+}
+
+export async function mockCreateAddress(
+  page: Page,
+  street: string,
+  city: string,
+  state: string = "New York",
+  postalCode: string
+) {
+  await page.route("**/library-card/api/address", async (route) => {
+    const addressData = {
+      line1: street,
+      city,
+      state,
+      zip: postalCode,
+      hasBeenValidated: true,
+    };
+
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify({
+        address: addressData,
+        addresses: [],
+        success: true,
+      }),
     });
   });
 }
