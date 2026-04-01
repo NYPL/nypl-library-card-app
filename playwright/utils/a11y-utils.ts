e02a1ad309361def93fd68e63e00d90ec126c9ef
Bridgette Rose <dz4888@wayne.edu>
2026-04-01 09:03:31 -0400

    add a11y-constant file (#631)
    * add a11y-constant file
    
    * updated constant
    
    * added helper for a11y tests
    
    * condensed one file a11y-utils
    
    * delete helper and add and expect to a11y-utils and type annotation

---
+++ playwright/utils/a11y-utils.ts
@@ -0,0 +1,23 @@
+import { expect } from "@playwright/test";
+
+export const A11Y_GUIDELINES = ["wcag21aa", "wcag22aa"] as const;
+
+type A11yResultGroup = { tags: string[] };
+type A11yResults = {
+  passes: A11yResultGroup[];
+  violations: A11yResultGroup[];
+  inapplicable: A11yResultGroup[];
+};
+
+// Fails if required WCAG tags were not exercised by the axe run
+export function validateA11yCoverage(results: A11yResults) {
+  const analyzedTags = new Set([
+    ...results.passes.flatMap((r) => r.tags),
+    ...results.violations.flatMap((r) => r.tags),
+    ...results.inapplicable.flatMap((r) => r.tags),
+  ]);
+
+  for (const guideline of A11Y_GUIDELINES) {
+    expect(analyzedTags.has(guideline)).toBe(true);
+  }
+}
