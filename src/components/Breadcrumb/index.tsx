c800cd0a1c573d7df121bcc2581f4440ba3d44cc
Millie Savalia <>
2026-04-07 16:58:17 -0400

    fix lang path

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,35 +1,28 @@
 import { Breadcrumbs as DSBreadcrumbs } from "@nypl/design-system-react-components";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const qaHref = "https://qa-www.nypl.org";
 const prodHref = "https://www.nypl.org";
-
 const Breadcrumbs = () => {
-  const { locale } = useRouter();
-
+  const {
+    query: { lang },
+  } = useRouter();
   const homeHref =
     typeof window !== "undefined" && window.location.hostname === "www.nypl.org"
       ? prodHref
       : qaHref;
-
-  const LocalizedLink = ({
-    href,
-    ...props
-  }: React.ComponentProps<typeof Link>) => (
-    <Link href={href} locale={locale} {...props} />
-  );
-
+  const localizedPath = (path: string) =>
+    lang && lang !== "en" ? `/${path}?lang=${lang}` : path;
   const breadcrumbsData = [
     { url: homeHref, text: "Home", replace: false },
-    { url: "/new", text: "Get A Library Card", replace: true },
+    { url: localizedPath("/new"), text: "Get A Library Card", replace: true },
   ];
-
   return (
     <DSBreadcrumbs
       variant="booksAndMore"
       breadcrumbsData={breadcrumbsData}
-      customLinkComponent={LocalizedLink}
+      customLinkComponent={Link}
     />
   );
 };
