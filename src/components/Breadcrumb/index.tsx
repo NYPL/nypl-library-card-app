e7050f173bac8ae19f159223a1a3246235e56a15
Millie Savalia <>
2026-04-07 16:25:04 -0400

    localizedLink

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,27 +1,35 @@
 import { Breadcrumbs as DSBreadcrumbs } from "@nypl/design-system-react-components";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const qaHref = "https://qa-www.nypl.org";
 const prodHref = "https://www.nypl.org";
+
 const Breadcrumbs = () => {
   const { locale } = useRouter();
+
   const homeHref =
     typeof window !== "undefined" && window.location.hostname === "www.nypl.org"
       ? prodHref
       : qaHref;
-  const localizedPath = (path: string) =>
-    locale && locale !== "en" ? `/${locale}${path}` : path;
+
+  const LocalizedLink = ({
+    href,
+    ...props
+  }: React.ComponentProps<typeof Link>) => (
+    <Link href={href} locale={locale} {...props} />
+  );
+
   const breadcrumbsData = [
-    // "/" redirects to "/library-card" url so we have to hardcode this here.
     { url: homeHref, text: "Home", replace: false },
-    { url: localizedPath("/new"), text: "Get A Library Card", replace: true },
+    { url: "/new", text: "Get A Library Card", replace: true },
   ];
+
   return (
     <DSBreadcrumbs
       variant="booksAndMore"
       breadcrumbsData={breadcrumbsData}
-      customLinkComponent={Link}
+      customLinkComponent={LocalizedLink}
     />
   );
 };
