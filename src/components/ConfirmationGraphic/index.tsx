107d7f556501dbfd3f087e091e699c700d541dd8
Jiayong <toxiapo@gmail.com>
2026-04-08 13:47:45 -0400

    SWIS-420: Patron name cutoff at 400 percent zoom level (#660)
    * swap member name and date position
    
    * more spaces

--- src/components/ConfirmationGraphic/index.tsx
+++ src/components/ConfirmationGraphic/index.tsx
@@ -42,13 +42,15 @@ const styles = {
     p: "0 2em 0 1.25em",
     gap: "0.5em 0.25em",
     gridTemplateColumns: "58% 30%",
-    gridTemplateRows: "20% 40% 20%",
+    gridTemplateRows: "20% 40% 14%",
     justifyContent: "center",
     alignItems: "center",
   },
   memberName: {
     color: "white",
-    fontSize: "clamp(0.8rem, 0.8rem + 0.4vw, 1.2rem)",
+    fontSize: "clamp(0.8rem, 0.8rem + 0.4vw, 1rem)",
+    lineHeight: 1.1,
+    gridColumn: "span 3",
   },
   logoItem: {
     color: "white",
@@ -118,7 +120,7 @@ const ConfirmationContainer = () => {
   }, [canvas]);
 
   return (
-    <Box sx={styles.outerBox}>
+    <Box className="card-container" sx={styles.outerBox}>
       <Box className="image-lion" sx={styles.imageLion}>
         <Image
           alt="NYPL Library Barcode Background"
@@ -127,10 +129,13 @@ const ConfirmationContainer = () => {
           width="939"
         />
         <Grid className="background-lion" sx={styles.backgroundLion}>
-          <GridItem id="member-name" sx={styles.memberName}>
-            {t("confirmation.graphic.memberName")}
-            <Box className="content" fontSize="1.6em">
-              {name}
+          <GridItem id="issued" sx={styles.issuedText}>
+            {t("confirmation.graphic.issued")}
+            <Box
+              className="content"
+              fontSize={"clamp(1rem, 1rem + 0.2vw, 1.6rem)"}
+            >
+              {new Date().toLocaleDateString()}
             </Box>
           </GridItem>
           <GridItem sx={styles.logoItem}>
@@ -149,13 +154,10 @@ const ConfirmationContainer = () => {
               {barcode}
             </Box>
           </Box>
-          <GridItem id="issued" sx={styles.issuedText}>
-            {t("confirmation.graphic.issued")}
-            <Box
-              className="content"
-              fontSize={"clamp(1rem, 1rem + 0.2vw, 1.6rem)"}
-            >
-              {new Date().toLocaleDateString()}
+          <GridItem id="member-name" sx={styles.memberName}>
+            {t("confirmation.graphic.memberName")}
+            <Box className="content" fontSize="1.6em">
+              {name}
             </Box>
           </GridItem>
         </Grid>
