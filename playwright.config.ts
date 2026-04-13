c450845cc6cf9ec90c8a138466a1404283b3ad9e
roseb89 <dz4888@wayne.edu>
2026-04-13 17:04:53 -0400

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,12 +42,7 @@ export default defineConfig({
 
     {
       name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-        launchOptions: {
-          args: ["--enable-full-keyboard-focus"],
-        },
-      },
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
