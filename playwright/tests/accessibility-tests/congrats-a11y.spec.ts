9988aaf6d6ee8f95a3f02f33b229414da893278b
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-05-07 12:36:29 -0600

    Refactor

--- playwright/tests/accessibility-tests/congrats-a11y.spec.ts
+++ playwright/tests/accessibility-tests/congrats-a11y.spec.ts
@@ -13,25 +13,61 @@ import { mockCreatePatronApi } from "../../utils/mock-api";
 
 test.describe("accessibility tests on congrats page", () => {
   let pageManager: PageManager;
+  let fullName: string;
 
   test.beforeEach(async ({ page }) => {
     pageManager = new PageManager(page);
+    fullName = `${TEST_PATRON.firstName} ${TEST_PATRON.lastName}`;
+    await page.goto(PAGE_ROUTES.REVIEW());
   });
 
-  test("does not have accessibility violations on page", async ({ page }) => {
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags([...A11Y_GUIDELINES])
-      .analyze();
-    validateA11yCoverage(accessibilityScanResults);
-    expect(accessibilityScanResults.violations).toHaveLength(0);
-  });
-
-  test("tabs forward through the page for metro/non-metro patron", async () => {
-    await test.step("submits mocked application", async () => {
-      await pageManager.page.goto(PAGE_ROUTES.REVIEW());
+  test.describe("displays congrats page for temporary patron", () => {
+    test.beforeEach(async ({ page }) => {
       await mockCreatePatronApi(
-        pageManager.congratsPage.page,
-        `${TEST_PATRON.firstName} ${TEST_PATRON.lastName}`,
+        page,
+        fullName,
+        TEST_BARCODE_NUMBER,
+        PATRON_TYPES.DIGITAL_TEMPORARY
+      );
+      await clickNextButton(
+        pageManager.reviewPage,
+        pageManager.reviewPage.submitButton,
+        pageManager.congratsPage.temporaryHeading
+      );
+    });
+
+    test("does not have accessibility violations on page", async ({ page }) => {
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .withTags([...A11Y_GUIDELINES])
+        .analyze();
+      validateA11yCoverage(accessibilityScanResults);
+      expect(accessibilityScanResults.violations).toHaveLength(0);
+    });
+
+    test("tabs forward through the page", async () => {
+      const congratsLocators = [
+        pageManager.congratsPage.locationsLink,
+        pageManager.congratsPage.photoIdAndProofOfAddressLink,
+        pageManager.congratsPage.learnMoreLink,
+        pageManager.congratsPage.getHelpEmailLink,
+        pageManager.congratsPage.loginLink,
+        pageManager.congratsPage.nyplLocationLink,
+        pageManager.congratsPage.findOutLibraryLink,
+        pageManager.congratsPage.discoverLink,
+      ];
+      await expect(pageManager.congratsPage.temporaryHeading).toBeFocused();
+      for (const locator of congratsLocators) {
+        await pageManager.congratsPage.page.keyboard.press("Tab");
+        await expect(locator).toBeFocused();
+      }
+    });
+  });
+
+  test.describe("displays congrats page for metro/non-metro patron", () => {
+    test.beforeEach(async ({ page }) => {
+      await mockCreatePatronApi(
+        page,
+        fullName,
         TEST_BARCODE_NUMBER,
         PATRON_TYPES.DIGITAL_METRO
       );
@@ -42,7 +78,15 @@ test.describe("accessibility tests on congrats page", () => {
       );
     });
 
-    await test.step("tabs forward through the page", async () => {
+    test("does not have accessibility violations on page", async ({ page }) => {
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .withTags([...A11Y_GUIDELINES])
+        .analyze();
+      validateA11yCoverage(accessibilityScanResults);
+      expect(accessibilityScanResults.violations).toHaveLength(0);
+    });
+
+    test("tabs forward through the page", async () => {
       const congratsLocators = [
         pageManager.congratsPage.locationsLink,
         pageManager.congratsPage.photoIdAndProofOfAddressLink,
@@ -55,41 +99,6 @@ test.describe("accessibility tests on congrats page", () => {
       await expect(
         pageManager.congratsPage.metroOrNonMetroHeading
       ).toBeFocused();
-      for (const locator of congratsLocators) {
-        await pageManager.congratsPage.page.keyboard.press("Tab");
-        await expect(locator).toBeFocused();
-      }
-    });
-  });
-
-  test("tabs forward through the page for temporary patron", async () => {
-    await test.step("submits mocked application", async () => {
-      await pageManager.page.goto(PAGE_ROUTES.REVIEW());
-      await mockCreatePatronApi(
-        pageManager.congratsPage.page,
-        `${TEST_PATRON.firstName} ${TEST_PATRON.lastName}`,
-        TEST_BARCODE_NUMBER,
-        PATRON_TYPES.DIGITAL_TEMPORARY
-      );
-      await clickNextButton(
-        pageManager.reviewPage,
-        pageManager.reviewPage.submitButton,
-        pageManager.congratsPage.temporaryHeading
-      );
-    });
-
-    await test.step("tabs forward through the page", async () => {
-      const congratsLocators = [
-        pageManager.congratsPage.locationsLink,
-        pageManager.congratsPage.photoIdAndProofOfAddressLink,
-        pageManager.congratsPage.learnMoreLink,
-        pageManager.congratsPage.getHelpEmailLink,
-        pageManager.congratsPage.loginLink,
-        pageManager.congratsPage.nyplLocationLink,
-        pageManager.congratsPage.findOutLibraryLink,
-        pageManager.congratsPage.discoverLink,
-      ];
-      await expect(pageManager.congratsPage.temporaryHeading).toBeFocused();
       for (const locator of congratsLocators) {
         await pageManager.congratsPage.page.keyboard.press("Tab");
         await expect(locator).toBeFocused();
