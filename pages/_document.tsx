73a4fa934bc6e12ce8eaf0e41cff88ca908074ee
Millie Savalia <milliedsavalia@yahoo.com>
2026-06-08 14:21:32 -0400

    script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -48,7 +48,7 @@ class MyDocument extends Document<DocumentProps> {
           {/* OptinMonster */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `<!-- This site is converting visitors into subscribers and customers with OptinMonster - https://optinmonster.com --> <script>(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,12468,1044);</script> <!-- / https://optinmonster.com -->`,
+              __html: `(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,12468,1044);`,
             }}
           />
         </Head>
